462b0207efe018f02fe0c40bd1bbb3db0561470d
Stefan Kebekus <kebekus@kde.org>
2001-02-02 16:04:35 +0000

    Fixed problems on big-endian machines
    svn path=/trunk/kdegraphics/kdvi/; revision=81054

--- pk.cpp
+++ pk.cpp
@@ -65,6 +65,29 @@ extern	char  *xmalloc (unsigned, const char *);
 extern void oops(QString message);
 extern void alloc_bitmap(bitmap *bitmap);
 
+
+// This table is used for changing the bit order in a byte. The
+// expression bitflp[byte] takes a byte in big endian and gives the
+// little endian equivalent of that.
+static const uchar bitflip[256] = {
+  0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48, 176, 112, 240,
+  8, 136, 72, 200, 40, 168, 104, 232, 24, 152, 88, 216, 56, 184, 120, 248,
+  4, 132, 68, 196, 36, 164, 100, 228, 20, 148, 84, 212, 52, 180, 116, 244,
+  12, 140, 76, 204, 44, 172, 108, 236, 28, 156, 92, 220, 60, 188, 124, 252,
+  2, 130, 66, 194, 34, 162, 98, 226, 18, 146, 82, 210, 50, 178, 114, 242,
+  10, 138, 74, 202, 42, 170, 106, 234, 26, 154, 90, 218, 58, 186, 122, 250,
+  6, 134, 70, 198, 38, 166, 102, 230, 22, 150, 86, 214, 54, 182, 118, 246,
+  14, 142, 78, 206, 46, 174, 110, 238, 30, 158, 94, 222, 62, 190, 126, 254,
+  1, 129, 65, 193, 33, 161, 97, 225, 17, 145, 81, 209, 49, 177, 113, 241,
+  9, 137, 73, 201, 41, 169, 105, 233, 25, 153, 89, 217, 57, 185, 121, 249,
+  5, 133, 69, 197, 37, 165, 101, 229, 21, 149, 85, 213, 53, 181, 117, 245,
+  13, 141, 77, 205, 45, 173, 109, 237, 29, 157, 93, 221, 61, 189, 125, 253,
+  3, 131, 67, 195, 35, 163, 99, 227, 19, 147, 83, 211, 51, 179, 115, 243,
+  11, 139, 75, 203, 43, 171, 107, 235, 27, 155, 91, 219, 59, 187, 123, 251,
+  7, 135, 71, 199, 39, 167, 103, 231, 23, 151, 87, 215, 55, 183, 119, 247,
+  15, 143, 79, 207, 47, 175, 111, 239, 31, 159, 95, 223, 63, 191, 127, 255
+};                                                                              
+
 BMUNIT	bit_masks[33] = {
 	0x0,		0x1,		0x3,		0x7,
 	0xf,		0x1f,		0x3f,		0x7f,
@@ -344,7 +367,19 @@ void font::read_PK_char(unsigned int ch)
       if (rows_left != 0 || h_bit != g->bitmap.w)
 	oops(QString(i18n("Bad pk file (%1), too many bits")).arg(fontname));
     }
+
+    // The data in the bitmap is now in the processor's bit order,
+    // that is, big endian. Since XWindows needs little endian, we
+    // need to change the bit order now.
+    register unsigned char* bitmapData = g->bitmap.bits;
+    register unsigned char* endOfData  = bitmapData + g->bitmap.bytes_wide*g->bitmap.h;
+    while(bitmapData < endOfData) {
+      *bitmapData = bitflip[*bitmapData];
+      bitmapData++;
+    }
+
   } else {
+
     // Routines for small Endian start here. This applies e.g. to
     // Intel and Alpha processors.
 

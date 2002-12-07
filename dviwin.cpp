32fa0b873dc266ba445fbb5c173d7f906159f2e2
Waldo Bastian <bastian@kde.org>
2002-12-07 23:15:01 +0000

    Proper quoting
    svn path=/branches/KDE_2_2_BRANCH/kdegraphics/kdvi/; revision=192699

--- dviwin.cpp
+++ dviwin.cpp
@@ -862,10 +862,10 @@ void dviWindow::mousePressEvent ( QMouseEvent * e )
 	QUrl DVI_Url(dviFile->filename);
 	QUrl Link_Url(DVI_Url, hyperLinkList[i].linkText, TRUE );
 	
-	KShellProcess proc;
-	proc << "kfmclient openURL " << Link_Url.toString();
-	proc.start(KProcess::Block);
-	//@@@ Set up a warning requester if the command failed?
+        QStringList args;
+        args << "openURL";
+        args << Link_Url.toString();
+        kapp->kdeinitExec("kfmclient", args);
       }
       break;
     }

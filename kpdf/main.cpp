ac4ae4be3b4d906de52f224b27814c95a7e18f75
Stephan Binner <binner@kde.org>
2004-06-21 19:03:29 +0000

    CVS_SILENT No punctuation in command line descriptions
    svn path=/trunk/kdegraphics/kpdf/; revision=322582

--- kpdf/main.cpp
+++ kpdf/main.cpp
@@ -11,7 +11,7 @@ static const char version[] = "v0.3";
 
 static KCmdLineOptions options[] =
 {
-    { "+[URL]", I18N_NOOP("Document to open."), 0 },
+    { "+[URL]", I18N_NOOP("Document to open"), 0 },
     KCmdLineLastOption
 };
 

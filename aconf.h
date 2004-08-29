5bbcd7f7712ec02aa4f189c4d616698f9d9aa32c
Albert Astals Cid <tsdgeos@terra.es>
2004-08-29 09:59:37 +0000

    #define HAVE_MKSTEMPS 1 as libkdefakes provides it Some more checks so that we don't have any undefined warning
    svn path=/trunk/kdegraphics/kpdf/; revision=342403

--- aconf.h
+++ aconf.h
@@ -1,6 +1,10 @@
+#define HAVE_FSEEK0 0 // define it to 0, if you have it, config.h will have it defined to 1
+                      // and that will be used
+
 #include <config.h>
 
 #define HAVE_FREETYPE_H HAVE_FREETYPE
 #define HAVE_FREETYPE_FREETYPE_H HAVE_FREETYPE
 #define OPI_SUPPORT 0
 #define TEXTOUT_WORD_LIST 0
+#define HAVE_MKSTEMPS 1 //libkdefakes provides it

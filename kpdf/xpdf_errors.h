d41e0ae37e7ccb1ff3b5a1ed23f75d3605043ea9
Andrew Coles <andrew.i.coles@googlemail.com>
2004-09-03 13:02:58 +0000

    CVS_SILENT
    Added missing copyright headers.
    
    svn path=/trunk/kdegraphics/kpdf/; revision=343706

--- kpdf/xpdf_errors.h
+++ kpdf/xpdf_errors.h
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Andrew Coles <andrew_coles@yahoo.co.uk>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+ 
 #ifndef XPDF_ERRORS_H
 #define XPDF_ERRORS_H
 

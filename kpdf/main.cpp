92da173cc4fcdc50d6460144af4b6e5637f8d5bf
Albert Astals Cid <tsdgeos@terra.es>
2004-11-01 18:12:55 +0000

    Version bump so i don't forget for kde 3.3.2 :-D
    svn path=/branches/KDE_3_3_BRANCH/kdegraphics/kpdf/; revision=359635

--- kpdf/main.cpp
+++ kpdf/main.cpp
@@ -7,7 +7,7 @@
 static const char description[] =
 I18N_NOOP("kpdf, a kde pdf viewer based on xpdf");
 
-static const char version[] = "0.3.1";
+static const char version[] = "0.3.2";
 
 static KCmdLineOptions options[] =
 {

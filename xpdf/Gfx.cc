5d394eefb45737f9e154423326ec6df1283fd739
Dirk Mueller <mueller@kde.org>
2004-12-22 11:59:46 +0000

    fix xpdf buffer overflow
    svn path=/trunk/kdegraphics/kpdf/; revision=372724

--- xpdf/Gfx.cc
+++ xpdf/Gfx.cc
@@ -2654,7 +2654,9 @@ void Gfx::doImage(Object *ref, Stream *str, GBool inlineImg) {
     haveMask = gFalse;
     dict->lookup("Mask", &maskObj);
     if (maskObj.isArray()) {
-      for (i = 0; i < maskObj.arrayGetLength(); ++i) {
+      for (i = 0;
+	   i < maskObj.arrayGetLength() && i < 2*gfxColorMaxComps;
+	   ++i) {
 	maskObj.arrayGet(i, &obj1);
 	maskColors[i] = obj1.getInt();
 	obj1.free();

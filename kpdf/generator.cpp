2fb64506decb94cf4c0e594a4eead9bee82d0c68
André Wöbbeking <woebbeking@kde.org>
2004-12-27 23:30:59 +0000

    workaround for unsermake: add empty generator.cpp which only includes generator.moc
    CCMAIL: coolo@kde.org
    
    obviously automake can handle this. is this a bug or a missing feature in unsermake?
    
    svn path=/branches/kpdf_experiments/kdegraphics/kpdf/; revision=373626

---
+++ kpdf/generator.cpp
@@ -0,0 +1,15 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Albert Astals Cid <tsdgeos@terra.es>            *
+ *   Copyright (C) 2004 by Enrico Ros <eros.kde@email.it>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+// local includes
+#include "generator.h"
+
+
+#include "generator.moc"

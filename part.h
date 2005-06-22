d461a5c915614a7a6ea6e0188c61abaee5fdfd06
Albert Astals Cid <tsdgeos@terra.es>
2005-06-22 19:29:20 +0000

    Backport of fix for comment #4 of 106771
    svn path=/branches/KDE/3.4/kdegraphics/kpdf/; revision=428029

--- part.h
+++ part.h
@@ -104,8 +104,6 @@ protected slots:
 	// can be connected to widget elements
 	void updateViewActions();
 	void enableTOC(bool enable);
-	
-	void openURLFromDocument(const KURL &url);
 
 public slots:
 	// connected to Shell action (and browserExtension), not local one
@@ -158,7 +156,6 @@ private:
 	bool m_actionsSearched;
 	bool m_searchStarted;
 	BrowserExtension *m_bExtension;
-	bool m_notifyOpening;
 };
 
 

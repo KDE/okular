24efac8722cc301f00ffbb8487f8ba88997f8c9d
Albert Astals Cid <tsdgeos@terra.es>
2005-08-02 17:08:48 +0000

    Backport fix for bug 110000
    svn path=/branches/KDE/3.4/kdegraphics/kpdf/; revision=442431

--- xpdf/xpdf/PDFDoc.cc
+++ xpdf/xpdf/PDFDoc.cc
@@ -115,24 +115,19 @@ PDFDoc::PDFDoc(BaseStream *strA, GString *ownerPassword,
 GBool PDFDoc::setup(GString *ownerPassword, GString *userPassword) {
   str->reset();
   
-  char eof[9];
+  char *eof = new char[1025];
   int pos = str->getPos();
-  str->setPos(8, -1);
-  eof[0] = str->getChar();
-  eof[1] = str->getChar();
-  eof[2] = str->getChar();
-  eof[3] = str->getChar();
-  eof[4] = str->getChar();
-  eof[5] = str->getChar();
-  eof[6] = str->getChar();
-  eof[7] = str->getChar();
-  eof[8] = '\0';
+  str->setPos(1024, -1);
+  for (int i = 0; i < 1024; i++) eof[i] = str->getChar();
+  eof[1024] = '\0';
   if (strstr(eof, "%%EOF") == NULL)
   {
     error(-1, "Document does not have ending %%EOF");	      
     errCode = errDamaged;
+    delete[] eof;
     return gFalse;
   }
+  delete[] eof;
   
   str->setPos(pos);
   

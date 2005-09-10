9a3f55c45aebdda41f6c8ab14e49046c0f92a7e4
Albert Astals Cid <tsdgeos@terra.es>
2005-09-10 12:01:26 +0000

    Do not launch the relayouting immediately but wait to return to the event loop as that relayouting interferes with observer loading
    svn path=/branches/KDE/3.5/kdegraphics/kpdf/; revision=459215

--- ui/pageview.cpp
+++ ui/pageview.cpp
@@ -272,7 +272,7 @@ void PageView::notifySetup( const QValueVector< KPDFPage * > & pageSet, bool doc
         // Need slotRelayoutPages() here instead of d->dirtyLayout = true
         // because opening a pdf from another pdf will not trigger a viewportchange
         // so pages are never relayouted
-        slotRelayoutPages();
+        QTimer::singleShot(0, this, SLOT(slotRelayoutPages()));
     else
     {
         // update the mouse cursor when closing because we may have close through a link and

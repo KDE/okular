7851c80d03730d9445df96e0a4845b8b0932a195
Dirk Mueller <mueller@kde.org>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdegraphics/kdvi/; revision=467420

--- kdvi_multipage_texthandling.cpp
+++ kdvi_multipage_texthandling.cpp
@@ -18,7 +18,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 // 02110-1301, USA.
 //
 // Please report bugs or improvements, etc. via the "Report bug"-Menu

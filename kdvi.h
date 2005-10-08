be492075ef16be1fa4800e72c4c86a53be974a57
Wilfried Huss <wilfried.huss@gmx.at>
2005-10-08 08:37:01 +0000

    SVN_SILENT add missing header guards
    svn path=/trunk/KDE/kdegraphics/kdvi/; revision=468464

--- kdvi.h
+++ kdvi.h
@@ -5,9 +5,13 @@
 //
 // (C) 2000, Stefan Kebekus. Distributed under the GPL.
 
+#ifndef KDVI_H
+#define KDVI_H
 
 // Define the following flags to generate debugging output
-  
+
 // #define DEBUG_FONT 1
 // #define DEBUG_FONTPOOL 1
 // #define DEBUG_PK 1
+
+#endif

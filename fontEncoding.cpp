f955505babb5dcb4cce7c33df5434b72c5d7bed2
Angus Leeming <leeming@lyx.org>
2005-10-19 22:43:31 +0000

    SVN_SILENT: Fix two typos in comments: s/Manuell/Manual/ s/environemt/environment/
    svn path=/branches/work/kviewshell-0.7/kviewshell/plugins/dvi/; revision=472136

--- fontEncoding.cpp
+++ fontEncoding.cpp
@@ -1,6 +1,6 @@
 // fontEncoding.cpp
 //
-// Part of KDVI - A DVI previewer for the KDE desktop environemt
+// Part of KDVI - A DVI previewer for the KDE desktop environment
 //
 // (C) 2003 Stefan Kebekus
 // Distributed under the GPL

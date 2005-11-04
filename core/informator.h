7176dd8b86851f0f6b7bbafa8a5e02c7ef54a75b
Piotr Szymanski <djurban@pld-dc.org>
2005-11-04 12:16:21 +0000

    - informatior thingie, actually just syncing my build checkkout with svn and will do a biggr clean up in a while
    svn path=/trunk/playground/graphics/oKular/kpdf/; revision=477589

---
+++ core/informator.h
@@ -0,0 +1,17 @@
+
+/**
+ * @short The source of information about the Generator
+ * 
+ * The goal of this class is to provide per Generator settings,
+ * about data and other stuff not related with generating oKular 
+ * content, but still needed for a full-featured plugin system.
+ *
+ */
+
+class Informator
+{
+    virtual QString iconName() = 0;
+    virtual void addConfigurationPages ( KConfigDialog* cfg ) = 0;
+    virtual KAboutData about();
+    virtual QStringList mimetypes() = 0;
+}

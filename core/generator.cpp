fec81c9e07c486c972530fdda51beacc27032c59
Piotr Szymanski <djurban@pld-dc.org>
2005-11-20 15:30:15 +0000

    - missed file
    svn path=/trunk/playground/graphics/oKular/kpdf/; revision=481786

---
+++ core/generator.cpp
@@ -0,0 +1,13 @@
+#include "generator.h"
+
+std::ostream& operator<< (std::ostream& str, const PixmapRequest *req)
+{
+    QString s("");
+    if (req->async)
+        s += "As";
+    else
+        s += "S";
+    s += QString ("ync PixmapRequest (id: %1) (%2x%3) ").arg(req->id,req->width,req->height);
+    s += QString("rot:%4, prio: %5, pageNo: %6) ").arg(req->rotation,req->priority,req->pageNumber);
+    return (str << s);
+}

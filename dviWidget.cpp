01c0aec7da08b7c5b439e3bee5c231b406ec512d
Wilfried Huss <wilfried.huss@gmx.at>
2006-02-28 20:50:22 +0000

    Port of commit 514606
    Control + Left Click" for source hyperlinks.
    "Middle Click" still works of course.
    
    fixes bug #122791
    
    svn path=/trunk/KDE/kdegraphics/kviewshell/plugins/dvi/; revision=514611

--- dviWidget.cpp
+++ dviWidget.cpp
@@ -30,9 +30,6 @@ DVIWidget::DVIWidget(PageView* sv, DocumentPageCache* cache, const char* name)
 
 void DVIWidget::mousePressEvent(QMouseEvent* e)
 {
-  // Call implementation from parent
-  DocumentWidget::mousePressEvent(e);
-
   // Safety check
   if (!pageNumber.isValid())
     return;
@@ -46,7 +43,10 @@ void DVIWidget::mousePressEvent(QMouseEvent* e)
   }
 
   // Check if the mouse is pressed on a source-hyperlink
-  if ((e->button() == Qt::MidButton) && (pageData->sourceHyperLinkList.size() > 0))
+  // source hyperlinks can be invoked with the Middle Mousebutton or alternatively
+  // with Control+Left Mousebutton
+  if ((e->button() == Qt::MidButton || (e->button() == Qt::LeftButton && (e->state() & Qt::ControlButton)))
+      && (pageData->sourceHyperLinkList.size() > 0))
   {
     int minIndex = 0;
     int minimum = 0;
@@ -73,6 +73,9 @@ void DVIWidget::mousePressEvent(QMouseEvent* e)
     emit(SRCLink(pageData->sourceHyperLinkList[minIndex].linkText, inverseMap(e->pos()), this));
     e->accept();
   }
+
+  // Call implementation from parent
+  DocumentWidget::mousePressEvent(e);
 }
 
 

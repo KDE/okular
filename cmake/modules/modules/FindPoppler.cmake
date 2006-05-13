ccccbc4b9970b34c3cf65db1e7ee1d28a76135d8
Pino Toscano <pino@kde.org>
2006-05-13 11:26:01 +0000

    adapt the FindPoppler to look for poppler-qt4 instead of poppler
    svn path=/branches/work/kde4/playground/graphics/cmake/; revision=540406

--- cmake/modules/modules/FindPoppler.cmake
+++ cmake/modules/modules/FindPoppler.cmake
@@ -10,14 +10,14 @@
 # in the FIND_PATH() and FIND_LIBRARY() calls
 INCLUDE(UsePkgConfig)
 
-PKGCONFIG(poppler _PopplerIncDir _PopplerLinkDir _PopplerLinkFlags _PopplerCflags)
+PKGCONFIG(poppler-qt4 _PopplerIncDir _PopplerLinkDir _PopplerLinkFlags _PopplerCflags)
 
 if(_PopplerLinkFlags)
   # find again pkg-config, to query it about poppler version
   FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS /usr/bin/ /usr/local/bin )
 
-  # query pkg-config asking for a poppler >= 0.5.1
-  EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.5.1 poppler RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+  # query pkg-config asking for a poppler-qt4 >= 0.5.1
+  EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.5.1 poppler-qt4 RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
   if(_return_VALUE STREQUAL "0")
     set(POPPLER_FOUND TRUE)
   endif(_return_VALUE STREQUAL "0")
@@ -27,11 +27,11 @@ if (POPPLER_FOUND)
   set(POPPLER_INCLUDE_DIR ${_PopplerIncDir})
   set(POPPLER_LIBRARY ${_PopplerLinkFlags})
   if (NOT POPPLER_FIND_QUIETLY)
-    message(STATUS "Found Poppler: ${POPPLER_LIBRARY}")
+    message(STATUS "Found Poppler-qt4: ${POPPLER_LIBRARY}")
   endif (NOT POPPLER_FIND_QUIETLY)
 else (POPPLER_FOUND)
   if (POPPLER_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find POPPLER")
+    message(FATAL_ERROR "Could NOT find Poppler-qt4")
   endif (POPPLER_FIND_REQUIRED)
 endif (POPPLER_FOUND)
 

512ac4f986db1cc8c48332a4bbccd34aa6d0f17e
Pino Toscano <pino@kde.org>
2006-05-28 18:16:28 +0000

    put the mime icon for every file (quick guessing basing on the filename only)
    svn path=/branches/work/kde4/playground/graphics/okular/; revision=545990

--- ui/embeddedfilesdialog.cpp
+++ ui/embeddedfilesdialog.cpp
@@ -11,8 +11,10 @@
 #include <QTreeWidget>
 
 #include <kfiledialog.h>
+#include <kicon.h>
 #include <klocale.h>
 #include <kmessagebox.h>
+#include <kmimetype.h>
 
 #include "core/document.h"
 #include "embeddedfilesdialog.h"
@@ -33,6 +35,8 @@ EmbeddedFilesDialog::EmbeddedFilesDialog(QWidget *parent, const KPDFDocument *do
 	{
 		QTreeWidgetItem *twi = new QTreeWidgetItem();
 		twi->setText(0, ef->name());
+		KMimeType::Ptr mime = KMimeType::findByPath( ef->name(), 0, true );
+		twi->setIcon(0, KIcon(mime->icon()));
 		twi->setText(1, ef->description());
 		twi->setText(2, KGlobal::locale()->formatDateTime( ef->creationDate(), false, true ));
 		twi->setText(3, KGlobal::locale()->formatDateTime( ef->modificationDate(), false, true ));

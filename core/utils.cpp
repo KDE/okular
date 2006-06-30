bca4b9eb77a7bf5874151a5701908065e23dd375
Pino Toscano <pino@kde.org>
2006-06-30 21:42:41 +0000

    Create a okularUtils namespace with utility functions for generators (and maybe not only for them).
    svn path=/trunk/playground/graphics/okular/; revision=556608

---
+++ core/utils.cpp
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Luigi Toscano <luigi.toscano@tiscali.it>        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "utils.h"
+
+namespace okularUtils
+{
+
+QRect rotateRect( QRect source, int width, int height, int orientation )
+{
+    QRect ret;
+
+    // adapt the coordinates of the boxes to the rotation
+    switch ( orientation )
+    {
+        case 1:
+            ret = QRect( width - source.y() - source.height(), source.x(),
+                         source.height(), source.width() );
+            break;
+        case 2:
+            ret = QRect( width - source.x() - source.width(), height - source.y() - source.height(),
+                         source.width(), source.height() );
+            break;
+        case 3:
+            ret = QRect( source.y(), height - source.x() - source.width(),
+                         source.height(), source.width() );
+            break;
+        case 0:  // no modifications
+        default: // other cases
+            ret = source;
+    }
+
+    return ret;
+}
+
+}
---
+++ core/utils.h
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Luigi Toscano <luigi.toscano@tiscali.it>        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef _OKULAR_UTILS_H_
+#define _OKULAR_UTILS_H_
+
+#include "okular_export.h"
+
+#include <qrect.h>
+
+/**
+ * Namespace with utility functions
+ */
+namespace okularUtils
+{
+    /**
+     * Rotate the rect \p source in the area \p width x \p height with the
+     * specified \p orientation .
+     */
+    QRect OKULAR_EXPORT rotateRect( QRect source, int width, int height, int orientation );
+};
+
+
+#endif

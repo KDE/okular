92e4c31899fd0d4cea67e612bf3d20d7cb3566c2
Pino Toscano <pino@kde.org>
2006-07-20 20:47:05 +0000

    these files were created by Piotr Szymanski
    svn path=/trunk/playground/graphics/okular/; revision=564684

--- core/informator.h
+++ core/informator.h
@@ -1,3 +1,11 @@
+/***************************************************************************
+ *   Copyright (C) 2005   by Piotr Szymanski <niedakh@gmail.com>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
 
 /**
  * @short The source of information about the Generator

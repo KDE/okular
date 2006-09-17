dc2c0bef27a9ea9a428bfc6ed53765c888c12d0c
Pino Toscano <pino@kde.org>
2006-09-17 16:09:10 +0000

    Rewrite almost completely the AnnotWindow so it uses less hacks and more Qt stuff; now it's a bit more clean
    svn path=/trunk/playground/graphics/okular/; revision=585647

--- ui/annotwindow.h
+++ ui/annotwindow.h
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2006 by Chu Xiaodong <xiaodongchu@gmail.com>            *
+ *   Copyright (C) 2006 by Pino Toscano <pino@kde.org>                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -10,73 +11,26 @@
 #ifndef _ANNOTWINDOW_H_
 #define _ANNOTWINDOW_H_
 
-#include <QtGui/qwidget.h>
+#include <qframe.h>
 
-class QFrame;
 class QTextEdit;
 class Annotation;
-class CloseButton;
+class MovableTitle;
 
-//this widget is for the titlebar and size griper
-class MouseBox : public QWidget
-{
-    Q_OBJECT
-    public:
-        MouseBox( QWidget * parent);
-    signals:
-        void paintevent(QPaintEvent *e);
-        void mousepressevent(QMouseEvent *e);
-        void mousemoveevent(QMouseEvent *e);
-        void mousereleaseevent(QMouseEvent *e);
-    private:
-        //QWidget * m_parent;
-    public:
-        QPoint pointpressed;
-    protected:
-        void paintEvent(QPaintEvent *e);
-        void mousePressEvent(QMouseEvent *e);
-        void mouseMoveEvent(QMouseEvent *e);
-        void mouseReleaseEvent(QMouseEvent *e);
-
-};
-
-class AnnotWindow : public QWidget
+class AnnotWindow : public QFrame
 {
     Q_OBJECT
     public:
         AnnotWindow( QWidget * parent, Annotation * annot);
         
     private:
-        QFrame *frame;
-        //QLabel *labelAnnotName;
-        //QLabel *labelTimeDate;
-        //QRect rcCloseBtn;
-        //QRect rcOptionBtn;
-        //QSizeGrip *resizer;
-        MouseBox* titleBox;
-        MouseBox* resizerBox;
-        CloseButton* btnClose;
-        MouseBox* btnOption;
-        QString modTime;
-        
-       // QRect titlerc;
-       // QRect sizegriprc;
-        
+        MovableTitle * m_title;
         QTextEdit *textEdit;
     public:
         Annotation* m_annot;
-    protected:
-        void paintEvent(QPaintEvent * e);
-        void resizeEvent ( QResizeEvent * e );
-//        void mousePressEvent(QMouseEvent * e);
-//        void mouseMoveEvent(QMouseEvent * e);
-    private slots:
-        void slotTitleMouseMove(QMouseEvent* e);
-        void slotResizerMouseMove(QMouseEvent* e);
-        void slotResizerPaint(QPaintEvent* e);
- //       void slotPaintOptionBtn(QPaintEvent* e);
-        void slotOptionBtn( QMouseEvent* e);
-        void slotsaveWindowText();
 
+    private slots:
+        void slotOptionBtn();
+        void slotsaveWindowText();
 };
 #endif

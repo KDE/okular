7cce98ac14bf03319304007e76dfce28a5caa03e
Albert Astals Cid <tsdgeos@terra.es>
2006-09-19 17:30:02 +0000

    increase version
    svn path=/branches/KDE/3.5/kdegraphics/kpdf/; revision=586460

--- shell/main.cpp
+++ shell/main.cpp
@@ -21,7 +21,7 @@
 static const char description[] =
 I18N_NOOP("kpdf, a kde pdf viewer based on xpdf");
 
-static const char version[] = "0.5.4";
+static const char version[] = "0.5.5";
 
 static KCmdLineOptions options[] =
 {

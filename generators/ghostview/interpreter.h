1b501cd8d2e8888052eab19cd36d8ab610cc9ddb
Pino Toscano <pino@kde.org>
2006-10-29 15:30:45 +0000

    add missing copyright statement
    svn path=/trunk/playground/graphics/okular/; revision=600108

--- generators/ghostview/interpreter.h
+++ generators/ghostview/interpreter.h
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *   Copyright (C) 2005   by Piotr Szymanski <niedakh@gmail.com>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #ifndef _OKULAR_INTERPETER_H_
 #define _OKULAR_INTERPETER_H_
 #include <qgs.h>

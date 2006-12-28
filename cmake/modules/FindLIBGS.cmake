451e99ece0ebb16a7dec8e798b3b5dd53bff374b
Pino Toscano <pino@kde.org>
2006-12-28 12:39:52 +0000

    use the cached value, and put copyrights Albert Astals Cide agreed on relicensing it as BSD.
    svn path=/trunk/playground/graphics/cmake/modules/; revision=617214

--- cmake/modules/FindLIBGS.cmake
+++ cmake/modules/FindLIBGS.cmake
@@ -4,36 +4,46 @@
 #  LIBGS_FOUND - system has libgs
 #  LIBGS_LIBRARY - Link this to use libgs
 #
+# Copyright (c) 2006, Albert Astals Cid, <aacid@kde.org>
+# Copyright (c) 2006, Pino Toscano, <toscano.pino@tiscali.it>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
 include(CheckLibraryExists)
 
-# reset vars
-set(LIBGS_LIBRARY)
+if (LIBGS_LIBRARY)
+  # in cache already
+  set(LIBGS_FOUND TRUE)
+else (LIBGS_LIBRARY)
 
-set(CMAKE_LIBRARY_PATH "${LIB_INSTALL_DIR}")
-find_library(LIBGS_LIBRARY NAMES gs gs-esp
-  PATHS
-  /usr/lib
-  /usr/local/lib
-  ${GNUWIN32_DIR}/lib
-)
+  set(CMAKE_LIBRARY_PATH "${LIB_INSTALL_DIR}")
+  find_library(LIBGS_LIBRARY NAMES gs gs-esp
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    ${GNUWIN32_DIR}/lib
+  )
 
-if(LIBGS_LIBRARY)
-  check_library_exists(${LIBGS_LIBRARY} gsapi_new_instance "" LIBGS_HAVE_GSAPI_NEW_INSTANCE)
+  if(LIBGS_LIBRARY)
+    check_library_exists(${LIBGS_LIBRARY} gsapi_new_instance "" LIBGS_HAVE_GSAPI_NEW_INSTANCE)
 
-  if(LIBGS_HAVE_GSAPI_NEW_INSTANCE)
-    set(LIBGS_FOUND TRUE)
-  endif(LIBGS_HAVE_GSAPI_NEW_INSTANCE)
-endif(LIBGS_LIBRARY)
-set(CMAKE_LIBRARY_PATH)
+    if(LIBGS_HAVE_GSAPI_NEW_INSTANCE)
+      set(LIBGS_FOUND TRUE)
+    endif(LIBGS_HAVE_GSAPI_NEW_INSTANCE)
+  endif(LIBGS_LIBRARY)
+  set(CMAKE_LIBRARY_PATH)
 
-if (LIBGS_FOUND)
-  if (NOT LIBGS_FIND_QUIETLY)
-    message(STATUS "Found the GS library: ${LIBGS_LIBRARY}")
-  endif (NOT LIBGS_FIND_QUIETLY)
-else (LIBGS_FOUND)
-  if (LIBGS_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find the GS library")
-  endif (LIBGS_FIND_REQUIRED)
-endif (LIBGS_FOUND)
+  if (LIBGS_FOUND)
+    if (NOT LIBGS_FIND_QUIETLY)
+      message(STATUS "Found the GS library: ${LIBGS_LIBRARY}")
+    endif (NOT LIBGS_FIND_QUIETLY)
+  else (LIBGS_FOUND)
+    if (LIBGS_FIND_REQUIRED)
+      message(FATAL_ERROR "Could NOT find the GS library")
+    endif (LIBGS_FIND_REQUIRED)
+  endif (LIBGS_FOUND)
 
-MARK_AS_ADVANCED(LIBGS_LIBRARY)
+    set(LIBGS_LIBRARY ${LIBGS_LIBRARY} CACHE INTERNAL "The GhostScript library")
+
+endif (LIBGS_LIBRARY)

29042586749f2bdd5bee199b39dc8e7c33fec36e
Albert Astals Cid <tsdgeos@terra.es>
2007-01-14 21:50:10 +0000

    I for one welcome our new overlord^Wmantainer
    svn path=/trunk/playground/graphics/okular/; revision=623465

--- shell/main.cpp
+++ shell/main.cpp
@@ -42,10 +42,11 @@ int main(int argc, char** argv)
         KAboutData::License_GPL,
         "(C) 2002 Wilco Greven, Christophe Devriese\n(C) 2004-2005 Albert Astals Cid, Enrico Ros\n(C) 2005 Piotr Szymanski");
 
-    about.addAuthor("Albert Astals Cid", I18N_NOOP("Current mantainer"), "aacid@kde.org");
+    about.addAuthor("Pino Toscano", I18N_NOOP("Current mantainer"), "pino@kde.org");
+    about.addAuthor("Tobias Koenig", I18N_NOOP("Lots of framework work, ODT and FictionBook backends"), "tokoe@kde.org");
+    about.addAuthor("Albert Astals Cid", I18N_NOOP("Former mantainer"), "aacid@kde.org");
     about.addAuthor("Piotr Szymanski", I18N_NOOP("Created okular from KPDF codebase"), "djurban@pld-dc.org");
     about.addAuthor("Enrico Ros", 0, "eros.kde@email.it");
-    about.addAuthor("Pino Toscano", I18N_NOOP("DjVu renderer, porting to KDE4"), "pino.toscano@kdemail.net");
     about.addAuthor("Wilco Greven", 0, "greven@kde.org");
     about.addAuthor("Christophe Devriese", 0, "oelewapperke@oelewapperke.org");
     about.addAuthor("Laurent Montel", 0, "montel@kde.org");

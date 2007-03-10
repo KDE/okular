24c56ef3d9610050c0cd5468885c6813a670eea8
Pino Toscano <pino@kde.org>
2007-03-10 13:15:49 +0000

    move most of the okular cmake configure stuff within the okular source tree
    svn path=/trunk/playground/graphics/okular/; revision=641188

---
+++ OkularConfigureChecks.cmake
@@ -0,0 +1,18 @@
+
+option(
+   OKULAR_FORCE_DRM
+   "Forces okular to check for DRM to decide if you can copy/print protected pdf. (default=no)"
+   OFF
+)
+if(OKULAR_FORCE_DRM STREQUAL "ON")
+   set(_OKULAR_FORCE_DRM 1)
+else(OKULAR_FORCE_DRM STREQUAL "ON")
+   set(_OKULAR_FORCE_DRM 0)
+endif(OKULAR_FORCE_DRM STREQUAL "ON")
+
+# at the end, output the configuration
+configure_file(
+   ${CMAKE_CURRENT_SOURCE_DIR}/config-okular.h.cmake
+   ${CMAKE_CURRENT_BINARY_DIR}/config-okular.h
+)
+

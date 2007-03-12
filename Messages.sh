88006a575bf9d9d0a1bd0b4a5b6aa8fc8d3ddade
Pino Toscano <pino@kde.org>
2007-03-12 00:26:41 +0000

    extract the messages of each generator into an own pot: this way it's easier to translate a single backend (or provide the translation for a custom one), but as drawback some strings are repeated in more than one pot
    svn path=/trunk/playground/graphics/okular/; revision=641620

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC *.rc */*.rc >> rc.cpp || exit 11
-$EXTRACTRC $(find . -name "*.ui") >> rc.cpp || exit 12
-$XGETTEXT $(find . -name "*.cpp" -o -name "*.h") -o $podir/okular.pot
+$EXTRACTRC $(find conf/ -name "*.ui") $(find core/ -name "*.ui") $(find ui/ -name "*.ui") $(ls . | grep -E '\.ui') >> rc.cpp || exit 12
+$XGETTEXT $(find conf/ -name "*.cpp" -o -name "*.h") $(find core/ -name "*.cpp" -o -name "*.h") $(find ui/ -name "*.cpp" -o -name "*.h") $(find shell/ -name "*.cpp" -o -name "*.h") $(ls . | grep -E '\.cpp$') $(ls . | grep -E '\.h$') -o $podir/okular.pot

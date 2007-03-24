6d75d1bff58f08f6b2dd87dfde763221376c21a5
Pino Toscano <pino@kde.org>
2007-03-24 22:50:32 +0000

    make sure to extract the annotation tool tooltips
    svn path=/trunk/playground/graphics/okular/; revision=646204

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 $EXTRACTRC *.rc */*.rc >> rc.cpp || exit 11
 $EXTRACTRC $(find conf/ -name "*.ui") $(find core/ -name "*.ui") $(find ui/ -name "*.ui") $(ls . | grep -E '\.ui') >> rc.cpp || exit 12
+$EXTRACTRC --tag=tooltip --context='Annotation tool' ui/data/tools.xml >> rc.cpp || exit 13
 $XGETTEXT $(find conf/ -name "*.cpp" -o -name "*.h") $(find core/ -name "*.cpp" -o -name "*.h") $(find ui/ -name "*.cpp" -o -name "*.h") $(find shell/ -name "*.cpp" -o -name "*.h") $(ls . | grep -E '\.cpp$') $(ls . | grep -E '\.h$') -o $podir/okular.pot

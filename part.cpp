55d3e7f3532f300616d85685e59f0305c86a7b29
Albert Astals Cid <tsdgeos@terra.es>
2007-07-04 09:35:08 +0000

    deprecated--
    svn path=/trunk/KDE/kdegraphics/okular/; revision=683148

--- part.cpp
+++ part.cpp
@@ -1194,7 +1194,8 @@ void Part::slotSaveFileAs()
                 return;
         }
 
-        if ( !KIO::NetAccess::file_copy( url(), saveUrl, -1, true ) )
+        KIO::Job *copyJob = KIO::file_copy( url(), saveUrl, -1, true);
+        if ( !KIO::NetAccess::synchronousRun( copyJob, widget() ) )
             KMessageBox::information( widget(), i18n("File could not be saved in '%1'. Try to save it to another location.", saveUrl.prettyUrl() ) );
     }
 }

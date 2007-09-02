5e266d95a98ddbfa54cce5a67db17c7c1d05076e
Albert Astals Cid <tsdgeos@terra.es>
2007-09-02 15:40:22 +0000

    Don't crash when we don't have a ctu. Fix imported from poppler BUGS: 149416
    svn path=/branches/KDE/3.5/kdegraphics/kpdf/; revision=707656

--- xpdf/xpdf/GfxFont.cc
+++ xpdf/xpdf/GfxFont.cc
@@ -1514,6 +1514,7 @@ Gushort *GfxCIDFont::getCodeToGIDMap(FoFiTrueType *ff, int *mapsizep) {
   int cidlen;
 
   *mapsizep = 0;
+  if (!ctu) return NULL;
 
   /* we use only unicode cmap */
   cmap = -1;

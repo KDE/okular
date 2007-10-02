cba7ce25cc9bcf53343a1ea8aa5b8791ac66aee8
Pino Toscano <pino@kde.org>
2007-10-02 22:48:39 +0000

    bump version to 0.5.92
    svn path=/trunk/KDE/kdegraphics/okular/; revision=720302

--- aboutdata.h
+++ aboutdata.h
@@ -18,7 +18,7 @@ inline KAboutData okularAboutData( const char* name, const char* iname )
         name, //"okular",
         "okular",
         ki18n(iname), //I18N_NOOP("okular"),
-        "0.5.91",
+        "0.5.92",
         ki18n("okular, a universal document viewer"),
         KAboutData::License_GPL,
         ki18n("(C) 2002 Wilco Greven, Christophe Devriese\n"

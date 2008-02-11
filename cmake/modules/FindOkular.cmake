ebdc32b46cc00a568a8e8a6ac633938a44a5fb5b
Pino Toscano <pino@kde.org>
2008-02-11 17:12:23 +0000

    polish up a bit: - lowercase the function calls - use FindPackageHandleStandardArgs - a bit of doc
    svn path=/trunk/KDE/kdegraphics/cmake/modules/; revision=773724

--- cmake/modules/FindOkular.cmake
+++ cmake/modules/FindOkular.cmake
@@ -1,52 +1,38 @@
 # - Find Okular
-# Find the Okular Generators includes and library
+# Find the Okular core library
 #
 # This module defines
-#  OKULAR_FOUND
-#  OKULAR_LIBRARIES
-#  OKULAR_INCLUDE_DIR
+#  OKULAR_FOUND - whether the Okular core library was found
+#  OKULAR_LIBRARIES - the Okular core library
+#  OKULAR_INCLUDE_DIR - the include path of the Okular core library
 #
 
-IF (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
+if (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
 
   # Already in cache
-  SET(OKULAR_FOUND TRUE)
+  set (OKULAR_FOUND TRUE)
 
-ELSE (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
+else (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
 
-  FIND_LIBRARY (OKULAR_LIBRARIES okularcore
+  find_library (OKULAR_LIBRARIES
+    NAMES
+    okularcore
     PATHS
+    ${LIB_INSTALL_DIR}
     ${KDE4_LIB_DIR}
   )
 
-  FIND_PATH (OKULAR_INCLUDE_DIR okular/core/document.h
+  find_path (OKULAR_INCLUDE_DIR
+    NAMES
+    okular/core/document.h
+    PATHS
+    ${INCLUDE_INSTALL_DIR}
     ${KDE4_INCLUDE_DIR}
   )
 
-  #
-  # everything necessary found?
-  #
-  IF (OKULAR_LIBRARIES AND OKULAR_INCLUDE_DIR)
-    SET (OKULAR_FOUND TRUE)
-  ELSE (OKULAR_LIBRARIES AND OKULAR_INCLUDE_DIR)
-    SET (OKULAR_FOUND FALSE)
-  ENDIF (OKULAR_LIBRARIES AND OKULAR_INCLUDE_DIR)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Okular DEFAULT_MSG OKULAR_LIBRARIES OKULAR_INCLUDE_DIR)
 
-  MARK_AS_ADVANCED(OKULAR_INCLUDE_DIR OKULAR_LIBRARIES)
+endif (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
 
-ENDIF (OKULAR_INCLUDE_DIR AND OKULAR_LIBRARIES)
-
-
-#
-# output status
-#
-IF (OKULAR_FOUND)
-     if (NOT Okular_FIND_QUIETLY)
-        MESSAGE (STATUS "Found Okular Generator: ${OKULAR_INCLUDE_DIR} ${OKULAR_LIBRARIES}")
-     endif(NOT Okular_FIND_QUIETLY)
-ELSE (OKULAR_FOUND)
-     if (Okular_FIND_REQUIRED)
-        MESSAGE (FATAL_ERROR "Okular Generator not found." )
-     endif (Okular_FIND_REQUIRED)
-ENDIF (OKULAR_FOUND)
-
+mark_as_advanced(OKULAR_INCLUDE_DIR OKULAR_LIBRARIES)

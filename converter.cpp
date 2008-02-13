20c82e7634c543217ebbeded77efa0f40b59ba16
Ely Levy <elylevy@cs.huji.ac.il>
2008-02-13 18:41:42 +0000

    added license
    svn path=/trunk/playground/graphics/okular/epub/; revision=774642

--- converter.cpp
+++ converter.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Ely Levy <elylevy@cs.huji.ac.il>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #include "converter.h"
 
 #include <QtGui/QAbstractTextDocumentLayout>
--- generator_epub.h
+++ generator_epub.h
@@ -1,3 +1,11 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Ely Levy <elylevy@cs.huji.ac.il>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
 #ifndef _OKULAR_GENERATOR_EPUB_H_
 #define _OKULAR_GENERATOR_EPUB_H_
 #include <okular/core/textdocumentgenerator.h>
@@ -7,21 +15,6 @@ class EPubGenerator : public Okular::TextDocumentGenerator
  public:
   EPubGenerator( QObject *parent, const QVariantList &args );
   ~EPubGenerator() {};
-  
-  /*        MagicGenerator( QObject *parent, const QVariantList &args );
-            ~MagicGenerator();
-            
-            bool loadDocument( const QString &fileName, QVector<Okular::Page*> &pages );
-            
-            bool canGeneratePixmap() const;
-            void generatePixmap( Okular::PixmapRequest *request );
-            
-            protected:
-            bool doCloseDocument();
-            
-            private:
-            MagicDocument mMagicDocument;
-  */
 };
 
 #endif

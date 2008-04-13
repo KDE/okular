5b3e32479a3e2cc07cf48d32d5e0d0716b1bb631
Pino Toscano <pino@kde.org>
2008-04-13 16:15:41 +0000

    properly recognise the current poppler head version
    svn path=/trunk/KDE/kdegraphics/okular/; revision=796459

--- generators/poppler/config-okular-poppler.h.cmake
+++ generators/poppler/config-okular-poppler.h.cmake
@@ -1,2 +1,5 @@
 /* Defined if we have the 0.7 version of the Poppler library */
 #cmakedefine HAVE_POPPLER_0_7 1
+
+/* Defined if we have the 0.9 version of the Poppler library */
+#cmakedefine HAVE_POPPLER_0_9 1

0ea8aa75a1935801a2e7ba3917f973165cd318ea
Angelo Naselli <anaselli@linux.it>
2008-04-15 21:19:55 +0000

    Added base kipi support
    svn path=/trunk/KDE/kdegraphics/cmake/modules/; revision=797438

--- cmake/modules/FindKIPI.cmake
+++ cmake/modules/FindKIPI.cmake
@@ -5,40 +5,39 @@
 #  KIPI_INCLUDE_DIR - the KIPI include directory
 #  KIPI_LIBRARIES - Link this to use the KIPI library
 
+if (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+  # Already in cache
+  set (KIPI_FOUND TRUE)
+else (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+  if (NOT WIN32)
+    include(UsePkgConfig)
+    pkgconfig(libkipi _kipiIncDir _kipiLinkDir kipiLinkFlags _kipiCflags)
+  else (NOT WIN32)
+    # if win32 we force find_path
+    set (_kipiIncDir "")
+  endif(NOT WIN32)
 
-FIND_PATH(KIPI_INCLUDE_DIR
-  NAMES
-  libkipi/interface.h
-  PATHS
-  ${KDE4_INCLUDE_DIR}
-  ${INCLUDE_INSTALL_DIR}
-  )
+  if(_kipiIncDir)
+    find_path(KIPI_INCLUDE_DIR libkipi/interface.h
+      ${_ddjvuIncDir}
+      ${KDE4_INCLUDE_DIR}
+      ${INCLUDE_INSTALL_DIR}
+      ${GNUWIN32_DIR}/include
+    )
 
-FIND_LIBRARY(KIPI_LIBRARY
-  NAMES
-  kipi
-  PATHS
-  ${KDE4_LIB_DIR}
-  ${LIB_INSTALL_DIR}
-  )
+    find_library(KIPI_LIBRARIES NAMES kipi
+      PATHS
+      ${_kipiLinkDir}
+      ${KDE4_LIB_DIR}
+      ${LIB_INSTALL_DIR}
+      ${GNUWIN32_DIR}/lib
+    )
+  endif(_kipiIncDir)
 
-set(KIPI_LIBRARIES ${KIPI_LIBRARY})
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Kipi DEFAULT_MSG KIPI_INCLUDE_DIR KIPI_LIBRARIES )
 
-if(KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
-  set(KIPI_FOUND TRUE)
-endif(KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+  mark_as_advanced(KIPI_INCLUDE_DIR KIPI_LIBRARIES)
 
-if(KIPI_FOUND)
-  if(NOT KIPI_FIND_QUIETLY)
-    message(STATUS "Found KIPI: ${KIPI_LIBRARIES}")
-  endif(NOT KIPI_FIND_QUIETLY)
-else(KIPI_FOUND)
-  if(KIPI_FIND_REQUIRED)
-    if(NOT KIPI_INCLUDE_DIR)
-      message(FATAL_ERROR "Could not find KIPI includes.")
-    endif(NOT KIPI_INCLUDE_DIR)
-    if(NOT KIPI_LIBRARIES)
-      message(FATAL_ERROR "Could not find KIPI library.")
-    endif(NOT KIPI_LIBRARIES)
-  endif(KIPI_FIND_REQUIRED)
-endif(KIPI_FOUND)
+endif (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
+

068f4a9d007ed627d2654b93f8f9f1b9f8e3f35c
Pino Toscano <pino@kde.org>
2008-04-20 09:49:34 +0000

    backport: - when the loading of a textdocument-based document fails, cleanup all the stuff eventually gathered from the converter - cleanup also the annotation position when closing a textdocument
    svn path=/branches/KDE/4.0/kdegraphics/okular/; revision=799078

--- core/textdocumentgenerator.cpp
+++ core/textdocumentgenerator.cpp
@@ -221,7 +221,22 @@ bool TextDocumentGenerator::loadDocument( const QString & fileName, QVector<Okul
     d->mDocument = d->mConverter->convert( fileName );
 
     if ( !d->mDocument )
+    {
+        // loading failed, cleanup all the stuff eventually gathered from the converter
+        d->mTitlePositions.clear();
+        Q_FOREACH ( const TextDocumentGeneratorPrivate::LinkPosition &linkPos, d->mLinkPositions )
+        {
+            delete linkPos.link;
+        }
+        d->mLinkPositions.clear();
+        Q_FOREACH ( const TextDocumentGeneratorPrivate::AnnotationPosition &annPos, d->mAnnotationPositions )
+        {
+            delete annPos.annotation;
+        }
+        d->mAnnotationPositions.clear();
+
         return false;
+    }
 
     d->generateTitleInfos();
     d->generateLinkInfos();
@@ -274,6 +289,7 @@ bool TextDocumentGenerator::doCloseDocument()
     d->mTitlePositions.clear();
     d->mLinkPositions.clear();
     d->mLinkInfos.clear();
+    d->mAnnotationPositions.clear();
     d->mAnnotationInfos.clear();
     // do not use clear() for the following two, otherwise they change type
     d->mDocumentInfo = Okular::DocumentInfo();

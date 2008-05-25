474d8d3d13c82d50a0b40a58910c4d5a450242d8
Urs Wolfer <uwolfer@kde.org>
2008-05-25 16:56:51 +0000

    disable icon which is not available
    svn path=/trunk/KDE/kdegraphics/okular/; revision=812519

--- conf/dlgperformance.cpp
+++ conf/dlgperformance.cpp
@@ -25,7 +25,7 @@ DlgPerformance::DlgPerformance( QWidget * parent )
     m_dlg->descLabel->setFont( labelFont );
 
     m_dlg->cpuLabel->setPixmap( BarIcon( "cpu", 32 ) );
-    m_dlg->memoryLabel->setPixmap( BarIcon( "kcmmemory", 32 ) );
+//     m_dlg->memoryLabel->setPixmap( BarIcon( "kcmmemory", 32 ) ); // TODO: enable again when proper icon is available
 
     connect( m_dlg->kcfg_MemoryLevel, SIGNAL( changed( int ) ), this, SLOT( radioGroup_changed( int ) ) );
 }

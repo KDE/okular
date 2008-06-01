d2fb47db86f4b2f8b2e78e54d6654dd2e4264083
Pino Toscano <pino@kde.org>
2008-06-01 10:11:16 +0000

    SVN_SILENT there is new stuff, so bump the version to 0.2
    svn path=/trunk/KDE/kdegraphics/okular/; revision=815130

--- generators/tiff/generator_tiff.cpp
+++ generators/tiff/generator_tiff.cpp
@@ -132,7 +132,7 @@ static KAboutData createAboutData()
          "okular_tiff",
          "okular_tiff",
          ki18n( "TIFF Backend" ),
-         "0.1.1",
+         "0.2",
          ki18n( "A TIFF backend" ),
          KAboutData::License_GPL,
          ki18n( "© 2006-2008 Pino Toscano" ),

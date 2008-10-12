b94c38a7d7e7568e338218196e21c94154f43f41
Pino Toscano <pino@kde.org>
2008-10-12 16:29:10 +0000

    Remove the "old" way of configuring the toolbars and just use the XMLGUI facilities. This makes the toolbar editing safer.
    CCBUGS: 168528, 171186
    
    svn path=/branches/KDE/4.1/kdegraphics/okular/; revision=870503

--- shell/shell.cpp
+++ shell/shell.cpp
@@ -24,7 +24,6 @@
 #include <kaction.h>
 #include <kapplication.h>
 #include <kcmdlineargs.h>
-#include <kedittoolbar.h>
 #include <kfiledialog.h>
 #include <kpluginloader.h>
 #include <kmessagebox.h>
@@ -76,7 +75,7 @@ void Shell::init()
       // tell the KParts::MainWindow that this is indeed the main widget
       setCentralWidget(m_part->widget());
       // and integrate the part's GUI with the shell's
-      setupGUI(Keys | Save);
+      setupGUI(Keys | ToolBar | Save);
       createGUI(m_part);
       m_showToolBarAction = static_cast<KToggleAction*>(toolBarMenuAction());
       m_doc = qobject_cast<KDocumentViewer*>(m_part);
@@ -171,7 +170,6 @@ void Shell::setupActions()
   setStandardToolBarMenuEnabled(true);
 
   m_showMenuBarAction = KStandardAction::showMenubar( this, SLOT( slotShowMenubar() ), actionCollection());
-  KStandardAction::configureToolbars(this, SLOT(optionsConfigureToolbars()), actionCollection());
   m_fullScreenAction = KStandardAction::fullScreen( this, SLOT( slotUpdateFullScreen() ), this,actionCollection() );
 }
 
@@ -246,20 +244,6 @@ void Shell::fileOpen()
     KUrl url = dlg.selectedUrl();
     if ( !url.isEmpty() )
         openUrl( url );
-}
-
-  void
-Shell::optionsConfigureToolbars()
-{
-  KEditToolBar dlg(factory(), this);
-  connect(&dlg, SIGNAL(newToolBarConfig()), this, SLOT(applyNewToolbarConfig()));
-  dlg.exec();
-}
-
-  void
-Shell::applyNewToolbarConfig()
-{
-  applyMainWindowSettings(KGlobal::config()->group("MainWindow"));
 }
 
 void Shell::slotQuit()

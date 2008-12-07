acf5922dd492222540973073d2e36fd40d519395
Albert Astals Cid <tsdgeos@terra.es>
2008-12-07 23:33:15 +0000

    correctly restore m_showMenuBarAction status on startup
    svn path=/trunk/KDE/kdegraphics/okular/; revision=894123

--- shell/shell.cpp
+++ shell/shell.cpp
@@ -87,6 +87,7 @@ void Shell::init()
     createGUI(m_part);
     m_showToolBarAction = static_cast<KToggleAction*>(toolBarMenuAction());
     m_doc = qobject_cast<KDocumentViewer*>(m_part);
+    m_showMenuBarAction->setChecked( menuBar()->isVisible() );
   }
 
   connect( this, SIGNAL( restoreDocument(const KConfigGroup&) ),m_part, SLOT( restoreDocument(const KConfigGroup&)));

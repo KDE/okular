9f3163ee61b9061a15b19621b316d1122804637a
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:52:50 +0000

    Extracting missing strings from .kcfg files.
    svn path=/trunk/KDE/kdegraphics/okular/; revision=951704

--- generators/spectre/Messages.sh
+++ generators/spectre/Messages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-$EXTRACTRC $(find conf/ -name "*.ui") >> rc_okular_ghostview.cpp || exit 11
+$EXTRACTRC $(find conf/ -name "*.ui" -o -name "*.kcfg") >> rc_okular_ghostview.cpp || exit 11
 $XGETTEXT $(find . -name "*.cpp" -o -name "*.h") -o $podir/okular_ghostview.pot

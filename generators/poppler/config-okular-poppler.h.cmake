3f473512171dcc9346fb9ebc4ea5d0c648447f8f
Pino Toscano <pino@kde.org>
2009-05-13 11:20:07 +0000

    add detection for poppler 0.11 (development version of poppler 0.12)
    svn path=/trunk/KDE/kdegraphics/okular/; revision=967387

--- generators/poppler/config-okular-poppler.h.cmake
+++ generators/poppler/config-okular-poppler.h.cmake
@@ -1,2 +1,5 @@
 /* Defined if we have the 0.9 version of the Poppler library */
 #cmakedefine HAVE_POPPLER_0_9 1
+
+/* Defined if we have the 0.11 version of the Poppler library */
+#cmakedefine HAVE_POPPLER_0_11 1

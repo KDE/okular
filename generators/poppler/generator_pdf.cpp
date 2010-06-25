d75e661e0950f9da6e26067a50c487eea4ecf11e
Pino Toscano <pino@kde.org>
2010-06-25 12:13:35 +0000

    bump okular version to 0.10.5 bump okular-poppler version to 0.3.1, as it got a bugfix
    svn path=/branches/KDE/4.4/kdegraphics/okular/; revision=1142683

--- generators/poppler/generator_pdf.cpp
+++ generators/poppler/generator_pdf.cpp
@@ -275,7 +275,7 @@ static KAboutData createAboutData()
          "okular_poppler",
          "okular_poppler",
          ki18n( "PDF Backend" ),
-         "0.3",
+         "0.3.1",
          ki18n( "A PDF file renderer" ),
          KAboutData::License_GPL,
          ki18n( "© 2005-2008 Albert Astals Cid" )

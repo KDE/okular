585ea3cd5487cced4746e8bec829dfab435cf330
Pino Toscano <pino@kde.org>
2010-07-28 14:58:43 +0000

    bump plugins versions to be in sync with kde 4.4 and, in case, consider more work being done
    svn path=/branches/KDE/4.5/kdegraphics/okular/; revision=1156086

--- generators/comicbook/generator_comicbook.cpp
+++ generators/comicbook/generator_comicbook.cpp
@@ -25,7 +25,7 @@ static KAboutData createAboutData()
          "okular_comicbook",
          "okular_comicbook",
          ki18n( "ComicBook Backend" ),
-         "0.1.2",
+         "0.2",
          ki18n( "A renderer for various comic book formats" ),
          KAboutData::License_GPL,
          ki18n( "© 2007-2008 Tobias Koenig" )

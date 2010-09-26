64063fe9968dd4830fa260698b0d1d1ac5f039eb
Albert Astals Cid <tsdgeos@terra.es>
2010-09-26 17:31:31 +0000

    Alex says FindOkular.cmake is not the way to go and that we need to install an OkularConfig.cmake
    That get_filename_component thing seems quite fragile and hacky to me but he is the expert :-)
    
    CCMAIL: neundorf@kde.org
    
    svn path=/trunk/KDE/kdegraphics/cmake/modules/; revision=1179984

---
+++ OkularConfig.cmake
@@ -0,0 +1,17 @@
+# get from the full path to OkularConfig.cmake up to the base dir dir:
+get_filename_component( _okularBaseDir  ${CMAKE_CURRENT_LIST_FILE} PATH)
+get_filename_component( _okularBaseDir  ${_okularBaseDir} PATH)
+get_filename_component( _okularBaseDir  ${_okularBaseDir} PATH)
+get_filename_component( _okularBaseDir  ${_okularBaseDir} PATH)
+
+
+# find the full paths to the library and the includes:
+find_path(OKULAR_INCLUDE_DIR okular/core/document.h
+          HINTS ${_okularBaseDir}/include
+          NO_DEFAULT_PATH)
+
+find_library(OKULAR_CORE_LIBRARY okularcore 
+             HINTS ${_okularBaseDir}/lib
+             NO_DEFAULT_PATH)
+
+set(OKULAR_LIBRARIES ${OKULAR_CORE_LIBRARY})

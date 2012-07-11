124da866e8f8fec0538fe31eb58b2a1e3394ae24
Mailson Menezes <mailson@gmail.com>
2012-07-10 21:48:30 -0300

    Add license header to tile manager files

--- core/tilesmanager.h
+++ core/tilesmanager.h
@@ -1,3 +1,11 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Mailson Menezes <mailson@gmail.com>             *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #ifndef _OKULAR_TILES_MANAGER_H_
 #define _OKULAR_TILES_MANAGER_H_
 

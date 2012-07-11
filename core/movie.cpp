4c8f0bc5def7c755ab810adec78826fd51f713b1
Tobias Koenig <tokoe@kde.org>
2012-07-11 16:47:17 +0200

    Fix temporary movie file names correctly
    Actually we don't need the source name included into the name of
    the temporary file. So don't do it to prevent bogus names.

--- core/movie.cpp
+++ core/movie.cpp
@@ -58,10 +58,7 @@ Movie::Movie( const QString& fileName, const QByteArray &data )
      * GStreamer backend). Storing the data in a temporary file works fine
      * though, not to mention, it releases much needed memory. (gamaral)
      */
-    QString relativeFileName = (fileName.startsWith("./") ? fileName.mid( 2 ) : fileName);
-    relativeFileName.replace( '/', '_' );
-    relativeFileName.replace( '\\', '_' );
-    d->m_tmp = new QTemporaryFile( QString( "%1/okrXXXXXX_%2" ).arg( QDir::tempPath() ).arg( relativeFileName ) );
+    d->m_tmp = new QTemporaryFile( QString( "%1/okrXXXXXX" ).arg( QDir::tempPath() ) );
     if ( d->m_tmp->open() ) {
        d->m_tmp->write( data );
        d->m_tmp->flush();

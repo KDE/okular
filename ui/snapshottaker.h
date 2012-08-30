88a1a1aa19715800ac89cd7dcd153f4182b14c64
Albert Astals Cid <aacid@kde.org>
2012-08-31 00:53:46 +0200

    Since Tobias ignores my emails i'll add the copyright header myself
    CCMAIL: tokoe@kde.org

--- ui/snapshottaker.h
+++ ui/snapshottaker.h
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Tobias Koening <tokoe@kde.org>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #ifndef SNAPSHOTTAKER_H
 #define SNAPSHOTTAKER_H
 

KDE/okular

commit 437ddad58802d59f8e377f72268db96e32d53419

Hide tiles manager within okularcore
Only the Tile class (tile.h) is visible from the outside

ui/pagepainter.cpp
@@ -31,7 +31,7 @@
 #include "guiutils.h"
 #include "settings.h"
 #include "core/observer.h"
-#include "core/tilesmanager_p.h"
+#include "core/tile.h"
 
 K_GLOBAL_STATIC_WITH_ARGS( QPixmap, busyPixmap, ( KIconLoader::global()->loadIcon("okular", KIconLoader::NoGroup, 32, KIconLoader::DefaultState, QStringList(), 0, true) ) )
 
@@ -86,9 +86,8 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
     }
     destPainter->fillRect( limits, backgroundColor );
 
-    Okular::TilesManager *tilesManager = ( pixID == PAGEVIEW_ID ) ? page->tilesManager() : 0;
     const QPixmap *pixmap = 0;
-    if ( !tilesManager )
+    if ( !page->hasTilesManager() )
     {
         /** 1 - RETRIEVE THE 'PAGE+ID' PIXMAP OR A SIMILAR 'PAGE' ONE **/
         pixmap = page->_o_nearestPixmap( pixID, scaledWidth, scaledHeight );
@@ -240,10 +239,10 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
     /** 4A -- REGULAR FLOW. PAINT PIXMAP NORMAL OR RESCALED USING GIVEN QPAINTER **/
     if ( !useBackBuffer )
     {
-        if ( tilesManager )
+        if ( page->hasTilesManager() )
         {
             const Okular::NormalizedRect normalizedLimits( limits, scaledWidth, scaledHeight );
-            QList<Okular::Tile> tiles = tilesManager->tilesAt( crop | normalizedLimits, false );
+            QList<Okular::Tile> tiles = page->tilesAt( crop | normalizedLimits, false );
             QList<Okular::Tile>::const_iterator tIt = tiles.constBegin(), tEnd = tiles.constEnd();
             while ( tIt != tEnd )
             {
@@ -293,13 +292,13 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
         else
             has_alpha = true;
 
-        if ( tilesManager )
+        if ( page->hasTilesManager() )
         {
             backImage = QImage( limits.width(), limits.height(), QImage::Format_ARGB32_Premultiplied );
             backImage.fill( paperColor.rgb() );
             QPainter p( &backImage );
             const Okular::NormalizedRect normalizedLimits( limits, scaledWidth, scaledHeight );
-            QList<Okular::Tile> tiles = tilesManager->tilesAt( normalizedLimits, false );
+            QList<Okular::Tile> tiles = page->tilesAt( normalizedLimits, false );
             QList<Okular::Tile>::const_iterator tIt = tiles.constBegin(), tEnd = tiles.constEnd();
             while ( tIt != tEnd )
             {

CMakeLists.txt
@@ -92,6 +92,7 @@ install( FILES
            core/sourcereference.h
            core/textdocumentgenerator.h
            core/textpage.h
+           core/tile.h
            core/utils.h
            core/version.h
            core/fileprinter.h

core/document.cpp
@@ -76,6 +76,7 @@
 #include "sourcereference.h"
 #include "sourcereference_p.h"
 #include "texteditors_p.h"
+#include "tile.h"
 #include "tilesmanager_p.h"
 #include "utils_p.h"
 #include "view.h"
@@ -270,7 +271,7 @@ void DocumentPrivate::cleanupPixmapMemory( qulonglong memoryToFree )
             if ( !p ) // No pixmap to remove
                 break;
 
-            TilesManager *tilesManager = m_pagesVector.at( p->page )->tilesManager();
+            TilesManager *tilesManager = m_pagesVector.at( p->page )->d->tilesManager();
             if ( tilesManager && tilesManager->totalMemory() > 0 )
             {
                 qulonglong memoryDiff = p->memory;
@@ -1053,7 +1054,7 @@ void DocumentPrivate::sendGeneratorRequest()
         }
 
         QRect requestRect = r->isTile() ? r->normalizedRect().geometry( r->width(), r->height() ) : QRect( 0, 0, r->width(), r->height() );
-        TilesManager *tilesManager = ( r->id() == PAGEVIEW_ID ) ? r->page()->tilesManager() : 0;
+        TilesManager *tilesManager = ( r->id() == PAGEVIEW_ID ) ? r->page()->d->tilesManager() : 0;
 
         // request only if page isn't already present or request has invalid id
         if ( ( !r->d->mForce && r->page()->hasPixmap( r->id(), r->width(), r->height(), r->normalizedRect() ) ) || r->id() <= 0 || r->id() >= MAX_OBSERVER_ID )
@@ -1161,7 +1162,7 @@ void DocumentPrivate::sendGeneratorRequest()
 
     // [MEM] preventive memory freeing
     qulonglong pixmapBytes = 0;
-    TilesManager * tm = ( request->id() == PAGEVIEW_ID ) ? request->page()->tilesManager() : 0;
+    TilesManager * tm = ( request->id() == PAGEVIEW_ID ) ? request->page()->d->tilesManager() : 0;
     if ( tm )
         pixmapBytes = tm->totalMemory();
     else
@@ -1293,7 +1294,7 @@ void DocumentPrivate::refreshPixmaps( int pageNumber )
         requestedPixmaps.push_back( p );
     }
 
-    TilesManager *tilesManager = page->tilesManager();
+    TilesManager *tilesManager = page->d->tilesManager();
     if ( tilesManager )
     {
         tilesManager->markDirty();
@@ -4065,7 +4066,7 @@ void DocumentPrivate::requestDone( PixmapRequest * req )
     {
         // [MEM] 1.2 append memory allocation descriptor to the FIFO
         qulonglong memoryBytes = 0;
-        const TilesManager *tm = ( req->id() == PAGEVIEW_ID ) ? req->page()->tilesManager() : 0;
+        const TilesManager *tm = ( req->id() == PAGEVIEW_ID ) ? req->page()->d->tilesManager() : 0;
         if ( tm )
             memoryBytes = tm->totalMemory();
         else

core/page.cpp
@@ -38,6 +38,7 @@
 #include "rotationjob_p.h"
 #include "textpage.h"
 #include "textpage_p.h"
+#include "tile.h"
 #include "tilesmanager_p.h"
 
 #include <limits>
@@ -89,7 +90,7 @@ PagePrivate::~PagePrivate()
 
 void PagePrivate::imageRotationDone( RotationJob * job )
 {
-    TilesManager *tm = ( job->id() == PAGEVIEW_ID ) ? m_page->tilesManager() : 0;
+    TilesManager *tm = ( job->id() == PAGEVIEW_ID ) ? m_tilesManager : 0;
     if ( tm )
     {
         QPixmap *pixmap = new QPixmap( QPixmap::fromImage( job->image() ) );
@@ -214,7 +215,7 @@ void Page::setBoundingBox( const NormalizedRect& bbox )
 
 bool Page::hasPixmap( int id, int width, int height, const NormalizedRect &rect ) const
 {
-    TilesManager *tm = ( id == PAGEVIEW_ID ) ? tilesManager() : 0;
+    TilesManager *tm = ( id == PAGEVIEW_ID ) ? d->m_tilesManager : 0;
     if ( tm )
     {
         if ( width != tm->width() || height != tm->height() )
@@ -492,7 +493,7 @@ QLinkedList< FormField * > Page::formFields() const
 void Page::setPixmap( int id, QPixmap *pixmap, const NormalizedRect &rect )
 {
     if ( d->m_rotation == Rotation0 ) {
-        TilesManager *tm = ( id == PAGEVIEW_ID ) ? tilesManager() : 0;
+        TilesManager *tm = ( id == PAGEVIEW_ID ) ? d->m_tilesManager : 0;
         if ( tm )
         {
             tm->setPixmap( pixmap, rect );
@@ -974,9 +975,22 @@ const QPixmap * Page::_o_nearestPixmap( int pixID, int w, int h ) const
     return pixmap;
 }
 
-TilesManager *Page::tilesManager() const
+bool Page::hasTilesManager() const
 {
-    return d->m_tilesManager;
+    return d->m_tilesManager != 0;
+}
+
+QList<Tile> Page::tilesAt( const NormalizedRect &rect, bool allowEmpty ) const
+{
+    if ( d->m_tilesManager )
+        return d->m_tilesManager->tilesAt( rect, allowEmpty );
+    else
+        return QList<Tile>();
+}
+
+TilesManager *PagePrivate::tilesManager() const
+{
+    return m_tilesManager;
 }
 
 void PagePrivate::setTilesManager( TilesManager *tm )

core/page.h
@@ -31,7 +31,7 @@ class PagePrivate;
 class PageTransition;
 class SourceReference;
 class TextSelection;
-class TilesManager;
+class Tile;
 
 /**
  * @short Collector for all the data belonging to a page.
@@ -362,11 +362,21 @@ class OKULAR_EXPORT Page
         void deleteAnnotations();
 
         /**
-         * Returns the tile manager for the PAGEVIEW_ID observer.
+         * Returns whether pixmaps for the PAGEVIEW_ID observer are handled by a
+         * tile manager.
          *
          * @since 0.16 (KDE 4.10)
          */
-        TilesManager *tilesManager() const;
+        bool hasTilesManager() const;
+
+        /**
+         * Returns a list of all tiles intersecting with @p rect.
+         *
+         * @param allowEmpty If false only tiles with a non null pixmap are returned
+         *
+         * @since 0.16 (KDE 4.10)
+         */
+        QList<Tile> tilesAt( const NormalizedRect &rect, bool allowEmpty = true ) const;
 
     private:
         PagePrivate* const d;

core/page_p.h
@@ -104,8 +104,9 @@ class PagePrivate
         void deleteTextSelections();
 
         /**
-         * Sets the tiles manager for the PAGEVIEW_ID observer
+         * Get/set the tiles manager for the PAGEVIEW_ID observer
          */
+        TilesManager *tilesManager() const;
         void setTilesManager( TilesManager *tm );
 
         class PixmapObject

core/tile.h
@@ -1,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Fabio D'Urso <fabiodurso@hotmail.it>            *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef _OKULAR_TILE_H_
+#define _OKULAR_TILE_H_
+
+#include "area.h"
+
+class QPixmap;
+
+namespace Okular {
+
+/**
+ * This class represents a rectangular portion of a page.
+ *
+ * @since 0.16 (KDE 4.10)
+ */
+class OKULAR_EXPORT Tile
+{
+    public:
+        Tile( const NormalizedRect &rect, QPixmap *pixmap, bool isValid );
+        Tile( const Tile &t );
+        ~Tile();
+
+        /**
+         * Location of the tile
+         */
+        NormalizedRect rect() const;
+
+        /**
+         * Pixmap (may also be NULL)
+         */
+        QPixmap * pixmap() const;
+
+        /**
+         * True if the pixmap is available and updated
+         */
+        bool isValid() const;
+
+        Tile & operator=( const Tile &t );
+
+    private:
+        class Private;
+        Private * d;
+};
+
+}
+
+#endif // _OKULAR_TILE_H_

core/tilesmanager.cpp
@@ -12,6 +12,8 @@
 #include <QtCore/qmath.h>
 #include <QList>
 
+#include "tile.h"
+
 #define TILES_MAXSIZE 2000000
 
 using namespace Okular;

commit 3af7937384762ad463d6c6cc53e63e3c0d61ddb5

pagepainter: Use tiles in PageView only
Fixes regression introduced by commit 437ddad58802d59f8e377f72268db96e32d53419

ui/pagepainter.cpp
@@ -87,8 +87,10 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
     }
     destPainter->fillRect( limits, backgroundColor );
 
+    const bool hasTilesManager = ( pixID == PAGEVIEW_ID && page->hasTilesManager() );
     const QPixmap *pixmap = 0;
-    if ( !page->hasTilesManager() )
+
+    if ( !hasTilesManager )
     {
         /** 1 - RETRIEVE THE 'PAGE+ID' PIXMAP OR A SIMILAR 'PAGE' ONE **/
         pixmap = page->_o_nearestPixmap( pixID, scaledWidth, scaledHeight );
@@ -240,7 +242,7 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
     /** 4A -- REGULAR FLOW. PAINT PIXMAP NORMAL OR RESCALED USING GIVEN QPAINTER **/
     if ( !useBackBuffer )
     {
-        if ( page->hasTilesManager() )
+        if ( hasTilesManager )
         {
             const Okular::NormalizedRect normalizedLimits( limits, scaledWidth, scaledHeight );
             const QList<Okular::Tile> tiles = page->tilesAt( crop | normalizedLimits, false );
@@ -293,7 +295,7 @@ void PagePainter::paintCroppedPageOnPainter( QPainter * destPainter, const Okula
         else
             has_alpha = true;
 
-        if ( page->hasTilesManager() )
+        if ( hasTilesManager )
         {
             backImage = QImage( limits.width(), limits.height(), QImage::Format_ARGB32_Premultiplied );
             backImage.fill( paperColor.rgb() );
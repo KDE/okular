8288959babbc63a261085ee4f1236e0cbacc6332
Albert Astals Cid <aacid@kde.org>
2013-03-26 22:46:38 +0100

    Add proper headers

--- tests/urldetecttest.cpp
+++ tests/urldetecttest.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *   Copyright (C) 2013 Jaydeep Solanki <jaydp17@gmail.com>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #include <qtest_kde.h>
 
 #include "../ui/url_utils.h"

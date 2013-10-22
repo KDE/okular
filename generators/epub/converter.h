b745d37b8b5200282c4e1bf6190f6ec54f2622d0
Jaydeep Solanki <jaydp17@gmail.com>
2013-10-22 05:33:17 +0530

    fix internal links

--- generators/epub/converter.h
+++ generators/epub/converter.h
@@ -30,10 +30,11 @@ namespace Epub {
 
       void _emitData(Okular::DocumentInfo::Key key, enum epub_metadata type); 
       void _handle_anchors(const QTextBlock &start, const QString &name);
+      void _insert_local_links(const QString &key, const QPair<int, int> &value);
       EpubDocument *mTextDocument;
 
       QHash<QString, QTextBlock> mSectionMap;
-      QHash<QString, QPair<int, int> > mLocalLinks;
+      QHash<QString, QVector< QPair<int, int> > > mLocalLinks;
     };
 }
 

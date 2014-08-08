125ee3178c683efbd2e47fab0313d99214d7b084
Albert Astals Cid <aacid@kde.org>
2014-08-08 22:39:53 +0200

    Don't you hate it when you forget files?

---
+++ shell/okular_main.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *   Copyright (C) 2002 by Wilco Greven <greven@kde.org>                   *
+ *   Copyright (C) 2003 by Christophe Devriese                             *
+ *                         <Christophe.Devriese@student.kuleuven.ac.be>    *
+ *   Copyright (C) 2003 by Laurent Montel <montel@kde.org>                 *
+ *   Copyright (C) 2003-2007 by Albert Astals Cid <aacid@kde.org>          *
+ *   Copyright (C) 2004 by Andy Goossens <andygoossens@telenet.be>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+class QString;
+class QStringList;
+
+namespace Okular
+{
+
+enum Status { Error, AttachedOtherProcess, Success };
+
+Status main(const QStringList &paths, const QString &serializedOptions);
+
+}
+
+/* kate: replace-tabs on; indent-width 4; */

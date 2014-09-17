be5af7304f2e9da67536bd4cd970f6e58c93bb19
Frederik Gladhorn <gladhorn@kde.org>
2014-09-17 19:46:42 +0200

    Add missing debug_comicbook.h

---
+++ generators/comicbook/debug_comicbook.h
@@ -0,0 +1,17 @@
+/***************************************************************************
+ *   Copyright (C) 2014 by Frederik Gladhorn <gladhorn@kde.org>            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef OKULAR_COMICBOOK_DEBUG_P_H
+#define OKULAR_COMICBOOK_DEBUG_P_H
+
+#include <QtCore/qloggingcategory.h>
+
+Q_DECLARE_LOGGING_CATEGORY(OkularComicbookDebug)
+
+#endif

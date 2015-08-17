e60890896146118973e1b0b824433f1b16527193
Alex Richardson <arichardson.kde@gmail.com>
2015-08-17 18:27:51 +0100

    Remove check for Qt < 4.7
    Useless as we currently require at least 5.2

--- core/fileprinter.cpp
+++ core/fileprinter.cpp
@@ -25,7 +25,6 @@
 #include <KProcess>
 #include <KShell>
 #include <QtCore/QDebug>
-#include <kdeversion.h>
 #include <QStandardPaths>
 
 #include "debug_p.h"
@@ -161,11 +160,9 @@ QList<int> FilePrinter::pageList( QPrinter &printer, int lastPage,
     if ( printer.printRange() == QPrinter::PageRange ) {
         startPage = printer.fromPage();
         endPage = printer.toPage();
-#if QT_VERSION >= KDE_MAKE_VERSION(4,7,0)
     } else if ( printer.printRange() == QPrinter::CurrentPage) {
         startPage = currentPage;
         endPage = currentPage;
-#endif
     } else { //AllPages
         startPage = 1;
         endPage = lastPage;

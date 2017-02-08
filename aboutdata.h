02ea499d9081266209641cf4b3df781939580b9c
Albert Astals Cid <aacid@kde.org>
2017-02-08 20:22:33 +0100

    I don't consider myself the maintainer

--- aboutdata.h
+++ aboutdata.h
@@ -35,7 +35,7 @@ inline KAboutData okularAboutData()
 
     about.addAuthor(QStringLiteral("Pino Toscano"), i18n("Former maintainer"), QStringLiteral("pino@kde.org"));
     about.addAuthor(QStringLiteral("Tobias Koenig"), i18n("Lots of framework work, ODT and FictionBook backends"), QStringLiteral("tokoe@kde.org"));
-    about.addAuthor(QStringLiteral("Albert Astals Cid"), i18n("Current maintainer"), QStringLiteral("aacid@kde.org"));
+    about.addAuthor(QStringLiteral("Albert Astals Cid"), i18n("Developer"), QStringLiteral("aacid@kde.org"));
     about.addAuthor(QStringLiteral("Piotr Szymanski"), i18n("Created Okular from KPDF codebase"), QStringLiteral("djurban@pld-dc.org"));
     about.addAuthor(QStringLiteral("Enrico Ros"), i18n("KPDF developer"), QStringLiteral("eros.kde@email.it"));
     about.addCredit(QStringLiteral("Eugene Trounev"), i18n("Annotations artwork"), QStringLiteral("eugene.trounev@gmail.com"));

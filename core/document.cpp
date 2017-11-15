04a2ab9220acadb4960b6641aeb44557b42caa0b
Eugene Istomin <e.istomin@con.ee>
2017-11-15 12:36:18 +0500

    rename xml to digest

--- core/document.cpp
+++ core/document.cpp
@@ -2185,7 +2185,7 @@ QString DocumentPrivate::docDataFileName(const QUrl &url, qint64 document_size)
         QDir().mkpath(docdataDir);
     }
     //QString newokularfile = docdataDir + QLatin1Char('/') + fn;
-    QString newokularfile = url.path() + QStringLiteral(".xml");
+    QString newokularfile = url.path() + QStringLiteral(".digest");
     // we don't want to accidentally migrate old files when running unit tests
     if (!QFile::exists( newokularfile ) && !QStandardPaths::isTestModeEnabled())
     {

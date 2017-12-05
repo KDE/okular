ee2418be1d26f26193d03e0ab3bf460e6852852d
David Faure <faure@kde.org>
2017-12-05 09:48:40 +0100

    Re-enable -DQT_NO_URL_CAST_FROM_STRING and fix the CHM backend.

--- generators/chm/generator_chm.cpp
+++ generators/chm/generator_chm.cpp
@@ -102,8 +102,8 @@ bool CHMGenerator::loadDocument( const QString & fileName, QVector< Okular::Page
     // fill m_urlPage and m_pageUrl
     QList<QUrl> pageList;
     m_file->enumerateFiles(pageList);
-    const QString home = m_file->homeUrl().toString();
-    if (home != QLatin1String("/"))
+    const QUrl home = m_file->homeUrl();
+    if (home.path() != QLatin1String("/"))
         pageList.prepend(home);
     m_pageUrl.resize(pageNum);
 

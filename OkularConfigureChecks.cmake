95bc29a76fc1f93eaabe5383d934644067dfc884
Albert Astals Cid <aacid@kde.org>
2018-05-30 10:55:17 +0200

    Force release of free memory
    This should not be needed, but i can totally reproduce that without it
    top is still reporting lots of memory used by Okular when it's really
    not (heaptrack reports memory freed correctly)
    
    BUGS: 394834

--- OkularConfigureChecks.cmake
+++ OkularConfigureChecks.cmake
@@ -10,6 +10,10 @@ else (OKULAR_FORCE_DRM)
    set(_OKULAR_FORCE_DRM 0)
 endif (OKULAR_FORCE_DRM)
 
+# Check whether malloc_trim(3) is supported.
+include(CheckSymbolExists)
+check_symbol_exists(malloc_trim "malloc.h" HAVE_MALLOC_TRIM)
+
 # at the end, output the configuration
 configure_file(
    ${CMAKE_CURRENT_SOURCE_DIR}/config-okular.h.cmake

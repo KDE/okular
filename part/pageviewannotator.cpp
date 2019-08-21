45fc7cd805388db537903ae3c2585c048c392506
Katarina Behrens <Katarina.Behrens@cib.de>
2019-08-21 17:09:19 +0200

    Read the certificates from configuration

--- part/pageviewannotator.cpp
+++ part/pageviewannotator.cpp
@@ -330,9 +330,7 @@ public:
 
     QList<Okular::Annotation *> end() override
     {
-        QStringList items;
-        items << "Cert1"
-              << "Cert2";
+            QStringList items = Okular::Settings::certificates();
 
             Okular::Annotation * ann = nullptr;
 

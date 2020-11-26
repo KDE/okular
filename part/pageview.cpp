a5df71a79aeab404f2bbc722e759e4b0d9dd0751
Albert Astals Cid <albert.astals.cid@kdab.com>
2020-11-26 12:53:04 +0100

    Sign -> Digitally Sign
    And remove from toolbar, it's not that important it should be there
    
    Also increase version

--- part/pageview.cpp
+++ part/pageview.cpp
@@ -717,8 +717,8 @@ void PageView::setupActions(KActionCollection *ac)
     d->aMouseModeMenu->setText(i18nc("@action", "Selection Tools"));
     ac->addAction(QStringLiteral("mouse_selecttools"), d->aMouseModeMenu);
 
-    d->aSignature = new QAction(QIcon::fromTheme(QStringLiteral("application-pkcs7-signature")), i18n("&Sign..."), this);
-    ac->addAction(QStringLiteral("mouse_sign"), d->aSignature);
+    d->aSignature = new QAction(QIcon::fromTheme(QStringLiteral("application-pkcs7-signature")), i18n("Digitally &Sign..."), this);
+    ac->addAction(QStringLiteral("add_digital_signature"), d->aSignature);
     connect(d->aSignature, &QAction::triggered, this, &PageView::slotSignature);
 
     // speak actions

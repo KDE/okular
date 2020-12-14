579ce171c3871e0b061b1018ce07e74c6d98660c
Albert Astals Cid <aacid@kde.org>
2020-12-14 15:42:34 +0100

    make the (C) line a less bad

--- generators/poppler/certificatetools.cpp
+++ generators/poppler/certificatetools.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019 by Bubli                                           *
+ *   Copyright (C) 2019 by Bubli <Katarina.Behrens@cib.de>                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *

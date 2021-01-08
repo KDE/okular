084631d11eddadb0802f0adbcd4a56c18ea68efc
Albert Astals Cid <aacid@kde.org>
2021-01-09 00:21:30 +0100

    Use more standard cmake functionality to search for discount
    Instead of rolling our own, that's harder to understand and buggy anyway
    
    BUGS: 431311

--- cmake/modules/FindDiscount.cmake
+++ cmake/modules/FindDiscount.cmake
@@ -2,9 +2,8 @@
 # Find the discount markdown library.
 #
 # This module defines
-#  discount_FOUND - whether the discount library was found
-#  discount_LIBRARIES - the discount library
-#  discount_INCLUDE_DIR - the include path of the discount library
+#  Discount_FOUND - whether the discount library was found
+#  PkgConfig::Discount - the discount target to link to
 
 # Copyright (c) 2017, Julian Wolff, <wolff@julianwolff.de>
 # Copyright (c) 2018, Sune Vuorela, <sune@kde.org>
@@ -12,30 +11,10 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+include(FindPackageHandleStandardArgs)
 
-if (discount_INCLUDE_DIR AND discount_LIBRARIES)
+find_package(PkgConfig REQUIRED)
 
-  # Already in cache
-  set (discount_FOUND TRUE)
+pkg_check_modules(Discount IMPORTED_TARGET "libmarkdown")
 
-else (discount_INCLUDE_DIR AND discount_LIBRARIES)
-
-  find_library (discount_LIBRARIES
-    NAMES markdown libmarkdown
-  )
-
-  find_path (discount_INCLUDE_DIR
-    NAMES mkdio.h
-  )
-
-  include (FindPackageHandleStandardArgs)
-  find_package_handle_standard_args (discount DEFAULT_MSG discount_LIBRARIES discount_INCLUDE_DIR)
-
-endif (discount_INCLUDE_DIR AND discount_LIBRARIES)
-
-mark_as_advanced(discount_INCLUDE_DIR discount_LIBRARIES)
-
-if (discount_FOUND)
-   add_library(discount::Lib UNKNOWN IMPORTED)
-   set_target_properties(discount::Lib PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${discount_INCLUDE_DIR} IMPORTED_LOCATION ${discount_LIBRARIES})
-endif()
+find_package_handle_standard_args(Discount DEFAULT_MSG Discount_LIBRARIES)

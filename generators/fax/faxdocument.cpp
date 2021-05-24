f9cd6c98680f32007b3221c2d54b1044cb080798
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-05-24 11:28:38 +0200

    Manually convert license files for fax generator

--- generators/fax/faxdocument.cpp
+++ generators/fax/faxdocument.cpp
@@ -1,11 +1,7 @@
-/***************************************************************************
- *   Copyright (C) 2008 by Tobias Koenig <tokoe@kde.org>                   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+/*
+    SPDX-FileCopyrightText: 2008 Tobias Koenig <tokoe@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #include "faxdocument.h"
 
--- generators/fax/faxexpand.h
+++ generators/fax/faxexpand.h
@@ -1,22 +1,12 @@
-/* Include file for fax routines
-   Copyright (C) 1990, 1995  Frank D. Cringle.
-   Copyright (C) 2005  Helge Deller <deller@kde.org>
+/*
+    Include file for fax routines
+    This file is part of viewfax - g3/g4 fax processing software.
 
-This file is part of viewfax - g3/g4 fax processing software.
+    SPDX-FileCopyrightText: 1990, 1995  Frank D. Cringle.
+    SPDX-FileCopyrightText: 2005  Helge Deller <deller@kde.org>
 
-viewfax is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. */
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef _faxexpand_h_
 #define _faxexpand_h_

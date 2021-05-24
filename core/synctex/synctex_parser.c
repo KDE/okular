3c5cfc8abcd909a3f7c39d3df266b35fe4c862b8
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-05-24 11:38:03 +0200

    Convert license files for core/synctex directory

--- core/synctex/synctex_parser.c
+++ core/synctex/synctex_parser.c
@@ -1,52 +1,24 @@
 /*
- Copyright (c) 2008-2017 jerome DOT laurens AT u-bourgogne DOT fr
+    SPDX-FileCopyrightText: 2008-2017 jerome DOT laurens AT u-bourgogne DOT fr
+    SPDX-License-Identifier: X11
 
- This file is part of the __SyncTeX__ package.
+    This file is part of the __SyncTeX__ package.
 
- [//]: # (Latest Revision: Fri Jul 14 16:20:41 UTC 2017)
- [//]: # (Version: 1.19)
+    [//]: # (Latest Revision: Fri Jul 14 16:20:41 UTC 2017)
+    [//]: # (Version: 1.19)
 
- See `synctex_parser_readme.md` for more details
+    See `synctex_parser_readme.md` for more details
 
- ## License
+    Acknowledgments:
+    ----------------
+    The author received useful remarks from the pdfTeX developers, especially Hahn The Thanh,
+    and significant help from XeTeX developer Jonathan Kew
 
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without
- restriction, including without limitation the rights to use,
- copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the
- Software is furnished to do so, subject to the following
- conditions:
-
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE
-
- Except as contained in this notice, the name of the copyright holder
- shall not be used in advertising or otherwise to promote the sale,
- use or other dealings in this Software without prior written
- authorization from the copyright holder.
-
- Acknowledgments:
- ----------------
- The author received useful remarks from the pdfTeX developers, especially Hahn The Thanh,
- and significant help from XeTeX developer Jonathan Kew
-
- Nota Bene:
- ----------
- If you include or use a significant part of the synctex package into a software,
- I would appreciate to be listed as contributor and see "SyncTeX" highlighted.
-
- */
+    Nota Bene:
+    ----------
+    If you include or use a significant part of the synctex package into a software,
+    I would appreciate to be listed as contributor and see "SyncTeX" highlighted.
+*/
 
 /*  We assume that high level application like pdf viewers will want
  *  to embed this code as is. We assume that they also have locale.h and setlocale.

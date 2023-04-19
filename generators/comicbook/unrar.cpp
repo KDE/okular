8b814d003075453cc006197bc2e371281ac3c004
Sune Vuorela <sune@vuorela.dk>
2023-04-19 10:58:37 +0200

    Deprecation fix. SkipEmptyParts

--- generators/comicbook/unrar.cpp
+++ generators/comicbook/unrar.cpp
@@ -46,7 +46,7 @@ static UnrarFlavour *detectUnrar(const QString &unrarPath, const QString &versio
     bool ok = proc.waitForFinished(-1);
     Q_UNUSED(ok)
     const QRegularExpression regex(QStringLiteral("[\r\n]"));
-    const QStringList lines = QString::fromLocal8Bit(proc.readAllStandardOutput()).split(regex, QString::SkipEmptyParts);
+    const QStringList lines = QString::fromLocal8Bit(proc.readAllStandardOutput()).split(regex, Qt::SkipEmptyParts);
     if (!lines.isEmpty()) {
         if (lines.first().startsWith(QLatin1String("UNRAR "))) {
             kind = new NonFreeUnrarFlavour();
@@ -151,7 +151,7 @@ QStringList Unrar::list()
     startSyncProcess(helper->kind->processListArgs(mFileName));
 
     const QRegularExpression regex(QStringLiteral("[\r\n]"));
-    QStringList listFiles = helper->kind->processListing(QString::fromLocal8Bit(mStdOutData).split(regex, QString::SkipEmptyParts));
+    QStringList listFiles = helper->kind->processListing(QString::fromLocal8Bit(mStdOutData).split(regex, Qt::SkipEmptyParts));
 
     QString subDir;
 

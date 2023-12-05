ce3af327091f1edf32872bc143017a29fac3441d
Carl Schwan <carl@carlschwan.eu>
2023-12-05 14:43:08 +0100

    Use KBookMarkWidgets and QtGui::Private::QX11Info in Qt6

--- shell/okular_main.cpp
+++ shell/okular_main.cpp
@@ -22,7 +22,11 @@
 
 #include "config-okular.h"
 #if HAVE_X11
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <private/qtx11extras_p.h>
+#else
 #include <QX11Info>
+#endif
 #endif
 #if HAVE_DBUS
 #include <QDBusConnectionInterface>

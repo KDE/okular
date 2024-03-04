8bcafd80bb7da18fe074ce1428ac4eb48d4bc19e
Sune Vuorela <sune@vuorela.dk>
2024-03-04 14:14:08 +0100

    Clean up TextSelection and remove unused parts

--- core/misc.cpp
+++ core/misc.cpp
@@ -16,12 +16,11 @@ class TextSelection::Private
 {
 public:
     int direction;
-    int it[2];
     NormalizedPoint cur[2];
 };
 
 TextSelection::TextSelection(const NormalizedPoint &start, const NormalizedPoint &end)
-    : d(new Private)
+    : d(std::make_unique<Private>())
 {
     if (end.y - start.y < 0 || (end.y - start.y == 0 && end.x - start.x < 0)) {
         d->direction = 1;
@@ -31,41 +30,9 @@ TextSelection::TextSelection(const NormalizedPoint &start, const NormalizedPoint
 
     d->cur[0] = start;
     d->cur[1] = end;
-    d->it[d->direction % 2] = -1;
-    d->it[(d->direction + 1) % 2] = -1;
 }
 
-TextSelection::~TextSelection()
-{
-    delete d;
-}
-
-void TextSelection::end(const NormalizedPoint &p)
-{
-    // changing direction as in 2b , assuming the bool->int conversion is correct
-    int dir1 = d->direction;
-    d->direction = (p.y - d->cur[0].y < 0 || (p.y - d->cur[0].y == 0 && p.x - d->cur[0].x < 0));
-    if (d->direction != dir1) {
-        qCDebug(OkularCoreDebug) << "changing direction in selection";
-    }
-
-    d->cur[1] = p;
-}
-
-void TextSelection::itE(int p)
-{
-    d->it[(d->direction + 1) % 2] = p;
-}
-
-void TextSelection::itB(int p)
-{
-    d->it[(d->direction) % 2] = p;
-}
-
-int TextSelection::direction() const
-{
-    return d->direction;
-}
+TextSelection::~TextSelection() = default;
 
 NormalizedPoint TextSelection::start() const
 {
@@ -75,14 +42,4 @@ NormalizedPoint TextSelection::start() const
 NormalizedPoint TextSelection::end() const
 {
     return d->cur[(d->direction + 1) % 2];
-}
-
-int TextSelection::itB() const
-{
-    return d->it[d->direction % 2];
-}
-
-int TextSelection::itE() const
-{
-    return d->it[(d->direction + 1) % 2];
 }

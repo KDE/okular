KDE/okular

commit ffd6426fbf3df284aa218ab14f680210bb49199c

Merge backend settings into main settings dialog
There seems to be no good reason for the separation, and me and others
seem to always start the wrong dialog first

autotests/parttest.cpp
@@ -260,13 +260,13 @@ void PartTest::testGeneratorPreferences()
     Okular::Part part(nullptr, dummyArgs);
 
     // Test that we don't crash while opening the dialog
-    dialog = part.slotGeneratorPreferences();
+    dialog = part.slotPreferences();
     qApp->processEvents();
     delete dialog; // closes the dialog and recursively destroys all widgets
 
     // Test that we don't crash while opening a new instance of the dialog
     // This catches attempts to reuse widgets that have been destroyed
-    dialog = part.slotGeneratorPreferences();
+    dialog = part.slotPreferences();
     qApp->processEvents();
     delete dialog;
 }

part/part.cpp
@@ -732,17 +732,6 @@ void Part::setupViewerActions()
         prefs->setText(i18n("Configure Viewer…"));
     }
 
-    QAction *genPrefs = new QAction(ac);
-    ac->addAction(QStringLiteral("options_configure_generators"), genPrefs);
-    if (m_embedMode == ViewerWidgetMode) {
-        genPrefs->setText(i18n("Configure Viewer Backends…"));
-    } else {
-        genPrefs->setText(i18n("Configure Backends…"));
-    }
-    genPrefs->setIcon(QIcon::fromTheme(QStringLiteral("configure")));
-    genPrefs->setEnabled(m_document->configurableGenerators() > 0);
-    connect(genPrefs, &QAction::triggered, this, &Part::slotGeneratorPreferences);
-
     m_printPreview = KStandardAction::printPreview(this, SLOT(slotPrintPreview()), ac);
     m_printPreview->setEnabled(false);
 
@@ -1231,27 +1220,6 @@ void Part::setWindowTitleFromDocument()
     }
 
     Q_EMIT setWindowCaption(title);
-}
-
-KConfigDialog *Part::slotGeneratorPreferences()
-{
-    // Create dialog
-    KConfigDialog *dialog = new Okular::BackendConfigDialog(m_pageView, QStringLiteral("generator_prefs"), Okular::Settings::self());
-    dialog->setAttribute(Qt::WA_DeleteOnClose);
-
-    if (m_embedMode == ViewerWidgetMode) {
-        dialog->setWindowTitle(i18n("Configure Viewer Backends"));
-    } else {
-        dialog->setWindowTitle(i18n("Configure Backends"));
-    }
-
-    m_document->fillConfigDialog(dialog);
-
-    // Show it
-    dialog->setWindowModality(Qt::ApplicationModal);
-    dialog->show();
-
-    return dialog;
 }
 
 void Part::notifySetup(const QList<Okular::Page *> & /*pages*/, int setupFlags)
@@ -3025,14 +2993,17 @@ void Part::checkNativeSaveDataLoss(bool *out_wontSaveForms, bool *out_wontSaveAn
     *out_wontSaveAnnotations = wontSaveAnnotations;
 }
 
-void Part::slotPreferences()
+KConfigDialog *Part::slotPreferences()
 {
     // Create dialog
     PreferencesDialog *dialog = new PreferencesDialog(m_pageView, Okular::Settings::self(), m_embedMode, m_document->editorCommandOverride());
+    m_document->fillConfigDialog(dialog);
+
     dialog->setAttribute(Qt::WA_DeleteOnClose);
 
     // Show it
     dialog->show();
+    return dialog;
 }
 
 void Part::slotToggleChangeColors()
@@ -3050,6 +3021,8 @@ void Part::slotAccessibilityPreferences()
 {
     // Create dialog
     PreferencesDialog *dialog = new PreferencesDialog(m_pageView, Okular::Settings::self(), m_embedMode, m_document->editorCommandOverride());
+    m_document->fillConfigDialog(dialog);
+
     dialog->setAttribute(Qt::WA_DeleteOnClose);
 
     // Show it
@@ -3061,6 +3034,8 @@ void Part::slotAnnotationPreferences()
 {
     // Create dialog
     PreferencesDialog *dialog = new PreferencesDialog(m_pageView, Okular::Settings::self(), m_embedMode, m_document->editorCommandOverride());
+    m_document->fillConfigDialog(dialog);
+
     dialog->setAttribute(Qt::WA_DeleteOnClose);
 
     // Show it

part/part.h
@@ -167,7 +167,7 @@ public Q_SLOTS: // dbus
     Q_SCRIPTABLE uint currentPage();
     Q_SCRIPTABLE QString currentDocument();
     Q_SCRIPTABLE QString documentMetaData(const QString &metaData) const;
-    Q_SCRIPTABLE void slotPreferences();
+    Q_SCRIPTABLE KConfigDialog *slotPreferences();
     Q_SCRIPTABLE void slotFind();
     Q_SCRIPTABLE void slotPrintPreview();
     Q_SCRIPTABLE void slotPreviousPage();
@@ -278,7 +278,6 @@ public Q_SLOTS:
     void slotFileDirty(const QString &);
     bool slotAttemptReload(bool oneShot = false, const QUrl &newUrl = QUrl());
     void psTransformEnded(int, QProcess::ExitStatus);
-    KConfigDialog *slotGeneratorPreferences();
 
     void errorMessage(const QString &message, int duration = 0);
     void warningMessage(const QString &message, int duration = -1);

core/document.cpp
@@ -4841,12 +4841,6 @@ void Document::fillConfigDialog(KConfigDialog *dialog)
         return;
     }
 
-    // We know it's a BackendConfigDialog, but check anyway
-    BackendConfigDialog *bcd = dynamic_cast<BackendConfigDialog *>(dialog);
-    if (!bcd) {
-        return;
-    }
-
     // ensure that we have all the generators with settings loaded
     QList<KPluginMetaData> offers = DocumentPrivate::configurableGenerators();
     d->loadServiceList(offers);
@@ -4865,12 +4859,6 @@ void Document::fillConfigDialog(KConfigDialog *dialog)
         if (iface) {
             iface->addPages(dialog);
             pagesAdded = true;
-
-            if (value.generator == d->m_generator) {
-                const int rowCount = bcd->thePageWidget()->model()->rowCount();
-                KPageView *view = bcd->thePageWidget();
-                view->setCurrentPage(view->model()->index(rowCount - 1, 0));
-            }
         }
     }
     if (pagesAdded) {

commit 3831e8d3dc6ecfd56e9ffa30f0a1c32991f18aa2

Fix opening preferences from dbus
Fallout from ffd6426fbf3df284aa218ab14f680210bb49199c

autotests/parttest.cpp
@@ -260,13 +260,13 @@ void PartTest::testGeneratorPreferences()
     Okular::Part part(nullptr, dummyArgs);
 
     // Test that we don't crash while opening the dialog
-    dialog = part.slotPreferences();
+    dialog = part.realSlotPreferences();
     qApp->processEvents();
     delete dialog; // closes the dialog and recursively destroys all widgets
 
     // Test that we don't crash while opening a new instance of the dialog
     // This catches attempts to reuse widgets that have been destroyed
-    dialog = part.slotPreferences();
+    dialog = part.realSlotPreferences();
     qApp->processEvents();
     delete dialog;
 }

part/part.cpp
@@ -2993,7 +2993,12 @@ void Part::checkNativeSaveDataLoss(bool *out_wontSaveForms, bool *out_wontSaveAn
     *out_wontSaveAnnotations = wontSaveAnnotations;
 }
 
-KConfigDialog *Part::slotPreferences()
+void Part::slotPreferences()
+{
+    (void)realSlotPreferences();
+}
+
+KConfigDialog *Part::realSlotPreferences()
 {
     // Create dialog
     PreferencesDialog *dialog = new PreferencesDialog(m_pageView, Okular::Settings::self(), m_embedMode, m_document->editorCommandOverride());

part/part.h
@@ -167,7 +167,7 @@ public Q_SLOTS: // dbus
     Q_SCRIPTABLE uint currentPage();
     Q_SCRIPTABLE QString currentDocument();
     Q_SCRIPTABLE QString documentMetaData(const QString &metaData) const;
-    Q_SCRIPTABLE KConfigDialog *slotPreferences();
+    Q_SCRIPTABLE void slotPreferences();
     Q_SCRIPTABLE void slotFind();
     Q_SCRIPTABLE void slotPrintPreview();
     Q_SCRIPTABLE void slotPreviousPage();
@@ -289,8 +289,10 @@ public Q_SLOTS:
 #endif
 
 private:
+    friend class PartTest;
     bool aboutToShowContextMenu(QMenu *menu, QAction *action, QMenu *contextMenu);
     void showMenu(const Okular::Page *page, const QPoint point, const QString &bookmarkTitle = QString(), const Okular::DocumentViewport &vp = DocumentViewport(), bool showTOCActions = false);
+    KConfigDialog *realSlotPreferences();
     /**
      * Searches the actionCollections of all KXMLGUIClients that were created by the same factory()
      * as this Part for a QAction that has both the specified name and the specified class.